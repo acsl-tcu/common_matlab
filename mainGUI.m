a681ee4e51fcc5913b979de77e00ccc97f65a702
YoshikiK-tcu <gg1912037@gmail.com>
2025-02-28 13:27:30 +0900

    変更

--- mainGUI.m
+++ mainGUI.m
@@ -1,6 +1,6 @@
 %% Initialize settings
 % set path
-clear all
+clear allq
 cf = pwd;
 
 if contains(mfilename('fullpath'), "mainGUI")
@@ -17,9 +17,9 @@ userpath('clear');
 %%
 % each method's arguments : app.time,app.cha,app.logger,app.env,app.agent,i
 clc
-SimBaseMode = ["SimHL","SimPointMass", "SimVehicle", "SimSuspendedLoad", "SimVoronoi", "SimFHL", "SimFHL_Servo", "SimLiDAR", "SimFT", "SimEL", "SimMPC_Koopman"];
-ExpBaseMode = ["ExpTestMotiveConnection", "ExpHL", "ExpFHL", "ExpFHL_Servo", "ExpFT", "ExpEL", "ExpMPC_Koopman"];
-fExp = 0;
-fDebug = 1; % 1: active : for debug function
+SimBaseMode = ["","SimHL","SimPointMass", "SimVehicle", "SimSuspendedLoad", "SimVoronoi", "SimFHL", "SimFHL_Servo", "SimLiDAR", "SimFT", "SimEL","SimMPC","SimMPC_Koopman","SimMCKMPC"];
+ExpBaseMode = ["","ExpTestMotiveConnection", "ExpHL", "ExpFHL", "ExpFHL_Servo", "ExpFT", "ExpEL","ExpMPC_Koopman"];
+fExp = 1;
+fDebug = 0; % 1: active : for debug function
 PInterval = 0.6; % sec : poling interval for emergency stop
-gui = SimExp(fExp, fDebug, PInterval);
+gui = SimExp(fExp, fDebug, PInterval); Controller_param.weight.QW = diag([10; 1; 1; 1; 1; 1]);  % 姿勢角，角速度　1,2刻み
