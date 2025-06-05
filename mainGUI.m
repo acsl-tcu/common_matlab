fae6b96a0a74c2a7b9829b1b058271dcc3c4244c
Shin-Aka <g2401201@tcu.ac.jp>
2025-06-05 15:00:03 +0900

    update

--- mainGUI.m
+++ mainGUI.m
@@ -26,7 +26,7 @@ userpath('clear');
 % each method's arguments : app.time,app.cha,app.logger,app.env,app.agent,i
 clc
 SimBaseMode = ["","SimHL","SimMPC","SimMPC_Koopman","SimMPC_HL","SimMPC_HLMC","SimMPC_KMC","SimMEC", "SimMPC_Koopman_komatsu"];
-ExpBaseMode = ["","ExpHL","ExpMPC_Koopman","ExpMPC_HL","ExpMPC_HLMC","ExpMPC_HLMC_HL","ExpMPC_Koopman_komatsu"];
+ExpBaseMode = ["","ExpHL","ExpMPC_Koopman","ExpMPC_HL","ExpMPC_HLMC","ExpMPC_HLMC_HL","ExpMPC_Koopman_komatsu","ExpMPC_Koopman_kyo"];
 % comment out :  "ExpFHL_Servo", "ExpFT", "ExpEL","ExpTestMotiveConnection", "ExpFHL","SimFHL",
 fExp = 1;
 fDebug = 1; % 1: active : for debug function
