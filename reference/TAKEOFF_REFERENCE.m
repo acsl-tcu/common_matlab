4dc9794d47e7ffe29375cec775c874e2486f759e
Shin-Aka <g2401201@tcu.ac.jp>
2025-06-10 18:24:15 +0900

    1

--- reference/TAKEOFF_REFERENCE.m
+++ reference/TAKEOFF_REFERENCE.m
@@ -5,11 +5,11 @@ classdef TAKEOFF_REFERENCE < handle
     base_time
     base_state
     ts
-    te = 10;
+    te = 5;
     zd = 0.6; % goal altitude
     result
-    th_offset
-    th_offset0 = 200;
+    th_offset=[];
+    th_offset0 = 300;
   end
 
   methods
