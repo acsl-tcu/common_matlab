3561cf555a26c9ee5d8f9905dc7931d6cbd08a6c
miyachi_acslPC <g2212040@tcu.ac.jp>
2025-06-12 15:54:54 +0900

    Update Sim_MEC.m

--- mode/Sim_MEC.m
+++ mode/Sim_MEC.m
@@ -7,7 +7,6 @@ cellfun(@(xx) addpath(xx), tmp, 'UniformOutput', false);
 close all hidden; clear ; clc;
 userpath('clear');
 end
-
 %%
 ts = 0; % initial time
 dt = 0.025; % sampling period
@@ -21,12 +20,12 @@ initial_state.p = arranged_position([0, 0], 1, 1, 0);
 initial_state.q = [1; 0; 0; 0];
 initial_state.v = [0; 0; 0];
 initial_state.w = [0; 0; 0];
- 
+
 agent = DRONE;
 agent.parameter = DRONE_PARAM("DIATONE","mass",0.4); % プラントモデルにモデル誤差を与える．DRONE_PARAMのパラメータを上書きしている．
 agent.plant = MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)); % Model_Quat13
-agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)),["p", "q"]));
 agent.sensor = DIRECT_SENSOR(agent, 0.0); % modeファイル内で回すとき
+agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)),["p", "q"]));
 
 agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[1,1,0.5]},"HL"});
 % agent.reference = TIME_VARYING_REFERENCE(agent,{"gen_ref_circle",{"freq",5,"init",[0;0;1],"radius",1.0},"HL"});
@@ -39,17 +38,13 @@ agent.cha_allocation.l.reference = "landing";
 % agent.cha_allocation = struct("reference",["time_varying"], ...
 %     "t",struct("reference",["takeoff"]),"l",struct("reference","landing"));%この書き方だとcontrollerのこの行より上のcha_allocation消える
 
-
 agent.controller.nominal=FUNCTIONAL_HLC(agent,Controller_FHL(dt));
 agent.controller.mec=FUNCTIONAL_MECKC(agent,Controller_FHLMECK(dt));
 agent.cha_allocation.controller=["nominal","mec"];%cha_allocationにコントローラー登録
-
-% agent.cha_allocation.reference = struct("reference",["time_varying"], ...
-%     "t",struct("reference",["takeoff"]),"l",struct("reference","landing"));%この書き方だとcontrollerのこの行より上のcha_allocation消える
 
 function dfunc(app)
 app.logger.plot({1, "p", "per"},"ax",app.UIAxes,"xrange",[app.time.ts,app.time.te]);
 app.logger.plot({1, "q", "s"},"ax",app.UIAxes2,"xrange",[app.time.ts,app.time.te]);
 app.logger.plot({1, "v", "er"},"ax",app.UIAxes3,"xrange",[app.time.ts,app.time.te]);
-app.logger.plot({1, "input", ""},"ax",app.UIAxes4,"xrange",[app.time.ts,app.time.t]);
+app.logger.plot({1, "input", ""},"ax",app.UIAxes4,"xrange",[app.time.ts,app.time.te]);
 end
