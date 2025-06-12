1d0fe61c3ce0dd33a001e3efe2d5ad050ea1d3ca
miyachi_acslPC <g2212040@tcu.ac.jp>
2025-06-12 11:56:44 +0900

    SimFHLのConnector~の0消さないとmainで回らなかった．SimMEC_Kのいらないところ削除

--- mode/SimMEC_K.m
+++ mode/SimMEC_K.m
@@ -22,20 +22,13 @@ clear; close all; clc;
     post_func = @(app) dfunc(app); % function working at the "draw button" pushed.
     motive = Connector_Natnet_sim(1, dt); % imitation of Motive camera (motion capture system)
     logger = LOGGER(1, size(ts:dt:te, 2), 0, [],[]); % instance of LOOGER class for data logging
-    % logger2 = LOGGER(1, size(ts:dt:te, 2), 0, [],[]); % instance of LOOGER class for data logging
     initial_state.p = arranged_position([0, 0], 1, 1, 0);
     initial_state.q = [1; 0; 0; 0];
     initial_state.v = [0; 0; 0];
     initial_state.w = [0; 0; 0];
     
-    %----------------------------
-    % agent(1) = ノミナルモデル → 通常のSimHLでの定義と同じ
-    % agent(2) = プラントモデル → あえてモデル誤差を与えたモデル
-    %----------------------------
-    
     agent = DRONE;
     
-    % agent.parameter.nominal = DRONE_PARAM("DIATONE");
     agent.parameter = DRONE_PARAM("DIATONE","mass",0.4); % プラントモデルにモデル誤差を与える．DRONE_PARAMのパラメータを上書きしている．
     agent.plant = MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)); % Model_Quat13
     
@@ -47,7 +40,7 @@ clear; close all; clc;
     agent.reference = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[1,1,0.5]},"HL"});
     % agent.reference = TIME_VARYING_REFERENCE(agent,{"gen_ref_p2p",{"freq",5,"init",[0;0;1],"radius",1.0},"HL"});
     
-    % agent.controller.nominal=FUNCTIONAL_HLC(agent,Controller_FHL(dt));
+    % agent.controller.nominal=FUNCTIONAL_HLC(agent,Controller_FHL(dt));%mainGUIに合わせるならこの3行必要
     % agent.controller.mec=FUNCTIONAL_MECKC(agent,Controller_FHLMECK(dt));
     % agent.cha_allocation.controller=["nominal","mec"];
 
@@ -66,24 +59,11 @@ clear; close all; clc;
         agent.controller.do(time,'f');
         agent.input_transform.do(time,'f');
         agent.plant.do(time,'f');
- 
-     
-        % agent.controller.mec.Pa_p_pre = Pa_estimator.state.p;    % 1. 現時刻のプラントの推定位置を「mecのコントローラ」内に格納
-        % agent.plant.do(time, 'f');%  xn[k] % 2.状態更新
-        % agent.controller.mec.Pn_p_cur = [agent.plant.state.p;agent.plant.state.q;agent.plant.state.v;agent.plant.state.w];
-        % agent.sensor.do(time, 'f'); % 2 hxa[k] % 4.センサ情報取得
-        % Pa_estimator = agent(2).estimator.do(time, 'f'); % 5. プラントの推定器を回し，情報を取得
-        % Pa_estimator = agent.estimator.do(time, 'f'); % 5.推定器を回し，情報を取得
-        % agent.controller.mec.Pa_p_cur = [Pa_estimator.state.p;Pa_estimator.state.q;Pa_estimator.state.v;Pa_estimator.state.w];
-        % agent.reference.do(time, 'f'); % 7. プラントのリファレンス更新
-        % Pn_controller = agent.controller.nominal.do(time, 'f'); % 10. ノミナルのコントローラを回し，情報を取得
-        % agent.controller.mec.Pn_u = Pn_controller.input; % 11. ノミナルのコントローラから得られた制御入力を，プラントの制御入力にコピー
-        % agent.controller.do(time, 'f');% 4, 5 du[k], u[k] % 12. プラントのコントローラ計算
-        % agent.plant.do(time, 'f');% 6 xa[k+1] % 13. プラントの状態更新
+
         logger.logging(time, 'f', agent);
         time.t = time.t + time.dt;
         all = toc;
-        disp([num2str(time.t)])
+        disp([num2str(time.t)]);
     end
     
     save("Data\test","logger")
@@ -94,7 +74,7 @@ clear; close all; clc;
 set(0,'defaultAxesFontSize', 10);
 set(0, 'DefaultLineLineWidth', 1.5);
 logger.plot({1, "p", "er"}, {1, "input", ""},"xrange",[time.ts,time.t],"fig_num",1,"row_col",[1 2]);
-% logger.save('HL_sim_test_1008_sigmoid');
+% logger.save('HL_sim_test_1008_sigmoid')
 app.logger = logger;
 result_plot(app)
 
