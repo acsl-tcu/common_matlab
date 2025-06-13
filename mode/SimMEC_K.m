423469e9569fbe94c9e9bcea0e1b988b33f50feb
miyachi_acslPC <g2212040@tcu.ac.jp>
2025-06-13 15:04:24 +0900

    入力をスラストと回転角に分けた，3次元図追加

--- mode/SimMEC_K.m
+++ mode/SimMEC_K.m
@@ -27,8 +27,8 @@ agent.plant = MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)); % Model
 agent.sensor = DIRECT_SENSOR(agent, 0.0); % modeファイル内で回すとき
 agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)),["p", "q"]));
 
-% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[1,1,0.5]},"HL"});
-agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_circle",{"freq",5,"init",[0;0;1],"radius",1.0},"HL"});
+agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[1,1,0.5]},"HL"});
+% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_circle",{"freq",5,"init",[0;0;1],"radius",1.0},"HL"});
 % agent.reference = TIME_VARYING_REFERENCE(agent,{"gen_ref_p2p",{"freq",5,"init",[0;0;1],"radius",1.0},"HL"});
 agent.reference.takeoff = TAKEOFF_REFERENCE(agent,[]);
 agent.reference.landing = LANDING_REFERENCE(agent,dt,0.1);
@@ -43,9 +43,11 @@ agent.controller.mec=FUNCTIONAL_MECKC(agent,Controller_FHLMECK(dt));
 agent.cha_allocation.controller=["nominal","mec"];%cha_allocationにコントローラー登録
 
 function dfunc(app)
-app.logger.plot({1, "p", "er"},"ax",app.UIAxes,"xrange",[app.time.ts,app.time.te]);
+app.logger.plot({1, "p", "er"},"ax",app.UIAxes,"xrange",[app.time.ts,app.time.te],"fig_num",1);
 app.logger.plot({1, "q", "e"},"ax",app.UIAxes2,"xrange",[app.time.ts,app.time.te]);
 app.logger.plot({1, "v", "er"},"ax",app.UIAxes3,"xrange",[app.time.ts,app.time.te]);
-app.logger.plot({1, "input", ""}, "xrange",[app.time.ts,app.time.te]);
-app.logger.plot({1, "p1-p2", "er"},"color", 0);
+app.logger.plot({1, "input1", ""}, "xrange",[app.time.ts,app.time.te]);
+app.logger.plot({1, "input2:4", ""}, "xrange",[app.time.ts,app.time.te]);
+app.logger.plot({1, "p1-p2", "er"},"color", 0,"fig_num",2);
+app.logger.plot({1, "p1-p2-p3", "er"},"fig_num",3);
 end
