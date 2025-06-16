811ca2de8c2c2cfeab9fbaad6b705d7c16b45e1d
YosukeKoseki <g2212044@tcu.ac.jp>
2025-06-16 18:56:01 +0900

    pitch変

--- sample/InputTransform_Thrust2Throttle_drone.m
+++ sample/InputTransform_Thrust2Throttle_drone.m
@@ -3,15 +3,15 @@ function u_trans_param = InputTransform_Thrust2Throttle_drone(varargin)
     % drone Prop. input
 
     %% transmitter system
-    u_trans_param.gain_tl =[600;600;600;40]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
-    u_trans_param.th_offset_tl = 335;         % offset 3s[1021] 4s[900]　発掘[926]
-    u_trans_param.gain = [600;600;600;40];
-    u_trans_param.th_offset = 335;
+    % u_trans_param.gain_tl =[600;600;600;40]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
+    % u_trans_param.th_offset_tl = 335;         % offset 3s[1021] 4s[900]　発掘[926]
+    % u_trans_param.gain = [600;600;600;40];
+    % u_trans_param.th_offset = 335;
 
-    % u_trans_param.gain_tl =[10;10;10;10]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
-    % u_trans_param.th_offset_tl = 100;         % offset 3s[1021] 4s[900]　発掘[926]
-    % u_trans_param.gain = [10;10;10;10];
-    % u_trans_param.th_offset = 100;
+    u_trans_param.gain_tl =[10;10;10;10]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
+    u_trans_param.th_offset_tl = 200;         % offset 3s[1021] 4s[900]　発掘[926]
+    u_trans_param.gain = [10;10;10;10];
+    u_trans_param.th_offset = 200;
 
     % u_trans_param.gain_SuspendedLoad =[500;500;500;100]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
     % u_trans_param.th_offset_SuspendedLoad = 450;         % offset 3s[1021] 4s[900]　発掘[926]
