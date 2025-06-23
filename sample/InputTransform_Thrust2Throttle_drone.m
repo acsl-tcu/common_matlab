e7bd53fd00aafc47626d53378a0bb650e5664bb5
YoshikiK-tcu <g2112022@tcu.ac.jp>
2025-06-23 15:05:44 +0900

    Suspended gain

--- sample/InputTransform_Thrust2Throttle_drone.m
+++ sample/InputTransform_Thrust2Throttle_drone.m
@@ -6,12 +6,19 @@ function u_trans_param = InputTransform_Thrust2Throttle_drone(varargin)
     % u_trans_param.gain_tl =[600;600;600;40]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
     % u_trans_param.th_offset_tl = 335;         % offset 3s[1021] 4s[900]　発掘[926]
     % u_trans_param.gain = [600;600;600;40];
-    % u_trans_param.th_offset = 335;
+    % % u_trans_param.th_offset = 335;
 
-    u_trans_param.gain_tl =[10;10;10;10]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
-    u_trans_param.th_offset_tl = 200;         % offset 3s[1021] 4s[900]　発掘[926]
-    u_trans_param.gain = [10;10;10;10];
-    u_trans_param.th_offset = 200;
+    %初期 
+    % u_trans_param.gain_tl =[10;10;10;10]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
+    % u_trans_param.th_offset_tl = 200;         % offset 3s[1021] 4s[900]　発掘[926]
+    % u_trans_param.gain = [10;10;10;10];
+    % u_trans_param.th_offset = 200;
+
+     %単機牽引用↓
+    u_trans_param.th_offset     = 331;%機体質量と釣り合うスロットルオフセット
+    u_trans_param.th_offset_tl  = 260;  %テークオフとランディング初期オフセット。
+    u_trans_param.gain          = [300;300;300;20];%　
+    u_trans_param.gain_tl       = [300;300;300;20];%　　
 
     % u_trans_param.gain_SuspendedLoad =[500;500;500;100]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
     % u_trans_param.th_offset_SuspendedLoad = 450;         % offset 3s[1021] 4s[900]　発掘[926]
