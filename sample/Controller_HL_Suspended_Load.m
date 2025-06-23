06a235725ee336850288a8c323681178cc62af3d
YoshikiK-tcu <g2112022@tcu.ac.jp>
2025-06-23 15:23:22 +0900

    Suspended lqrd

--- sample/Controller_HL_Suspended_Load.m
+++ sample/Controller_HL_Suspended_Load.m
@@ -17,14 +17,17 @@ function Controller = Controller_HL_Suspended_Load(dt,agent)
         Controller.F4 = lqrd(A2,B2,diag([10,1]),0.1,dt);                        % yaw方向サブシステム, h4^(i) i = 0~1
     else
         % exp用
+        %common matlab 初期
+        % Controller.F1 = lqrd([0 1;0 0],[0;1],diag([100,1]),[1],dt);             %位置z、速度z
+        % Controller.F2 = lqrd(A6,B6,diag([1000,100,10,1,0.001,0.001]),0.1,dt);%より制御周期速くなると0.008とかが良くなると思う．
+        % Controller.F3 = lqrd(A6,B6,diag([1000,100,10,1,0.001,0.001]),0.1,dt);
+        % Controller.F4 = lqrd([0 1;0 0],[0;1],diag([10,1]),[10],dt);                %yawの位置、速度、100,1,1
+
+        %drone デフォルト        
         Controller.F1 = lqrd([0 1;0 0],[0;1],diag([100,1]),[1],dt);             %位置z、速度z
-        Controller.F2 = lqrd(A6,B6,diag([1000,100,10,1,0.001,0.001]),0.1,dt);%より制御周期速くなると0.008とかが良くなると思う．
-        Controller.F3 = lqrd(A6,B6,diag([1000,100,10,1,0.001,0.001]),0.1,dt);
-        Controller.F4 = lqrd([0 1;0 0],[0;1],diag([10,1]),[10],dt);                %yawの位置、速度、100,1,1
-% Controller.F1 = lqrd([0 1;0 0],[0;1],diag([100,1]),[1],dt);             %位置z、速度z
-%         Controller.F2 = lqrd(A6,B6,diag([150000,250000,10000,1,0.001,0.001]),0.08,dt);%より制御周期速くなると0.008とかが良くなると思う．
-%         Controller.F3 = lqrd(A6,B6,diag([150000,250000,10000,1,0.001,0.001]),0.08,dt);
-%         Controller.F4 = lqrd([0 1;0 0],[0;1],diag([10,1]),[1],dt);                %yawの位置、速度、100,1,1
+        Controller.F2 = lqrd(A6,B6,diag([150000,250000,10000,1,0.001,0.001]),0.08,dt);%より制御周期速くなると0.008とかが良くなると思う．
+        Controller.F3 = lqrd(A6,B6,diag([150000,250000,10000,1,0.001,0.001]),0.08,dt);
+        Controller.F4 = lqrd([0 1;0 0],[0;1],diag([10,1]),[1],dt);                %yawの位置、速度、100,1,1
 
  %        %↓ゲイン調整・確認用
  %        At = A6;
