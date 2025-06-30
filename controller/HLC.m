035fbe1ff17e9700236e63b2b5b28964359cae8e
miyachi_acslPC <g2212040@tcu.ac.jp>
2025-06-30 11:40:26 +0900

    Δu全要素に追加

--- controller/HLC.m
+++ controller/HLC.m
@@ -52,17 +52,21 @@ classdef HLC < handle
       % max,min are applied for the safty
       obj.result.nominal = [max(0,min(10,tmp(1)));max(-1,min(1,tmp(2)));max(-1,min(1,tmp(3)));max(-1,min(1,tmp(4)))];
       
-      if (norm(model.state.v) < 0.8) && (norm(model.state.p-ref.state.p)<0.2)
+      if (norm(model.state.v) < 0.8) && (norm(model.state.p-ref.state.p)<0.2)%安全条件
       % %スイープ周波数追加
       %chirp(初期時刻，初期周波数，指定時間，指定周波数)※周波数は指定周波数÷指定時間の割合で増えていく
-      obj.result.delta_u_thrust = 2.0*chirp(varargin{1}.t,0,varargin{1}.te,2,'linear')*[1;0;0;0];%スラスト
-      obj.result.delta_u_torque = 0.3*chirp(varargin{1}.t,0,varargin{1}.te,0.03,'linear')*[0;1;1;1];%トルク
+      obj.result.delta_u_thrust = 2.0*chirp(varargin{1}.t,0,varargin{1}.te,2,'linear')*[1;0;0;0];%thrust
+      obj.result.delta_u_roll = 0.3*chirp(varargin{1}.t,0,varargin{1}.te,0.03,'linear')*[0;1;0;0];%roll
+      obj.result.delta_u_pitch = 0.3*chirp(varargin{1}.t,0,varargin{1}.te,0.03,'linear')*[0;0;1;0];%pitch
+      obj.result.delta_u_yaw = 0.3*chirp(varargin{1}.t,0,varargin{1}.te,0.03,'linear')*[0;0;0;1];%yaw
       else
       obj.result.delta_u_thrust = 0;
-      obj.result.delta_u_torque = 0;
+      obj.result.delta_u_roll = 0;
+      obj.result.delta_u_pitch = 0;
+      obj.result.delta_u_yaw = 0;
       end
       
-      obj.result.delta_u = obj.result.delta_u_thrust+obj.result.delta_u_torque;
+      obj.result.delta_u = obj.result.delta_u_thrust+obj.result.delta_u_roll+obj.result.delta_u_pitch+obj.result.delta_u_yaw;
       obj.result.input = obj.result.delta_u+obj.result.nominal;
       result = obj.result;
     end
