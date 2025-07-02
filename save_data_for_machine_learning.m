dc472a686ab02671ca1dbac3016dac65824bccfb
1912063 <g2212044@tcu.ac.jp>
2025-07-02 13:19:38 +0900

    save_data_for_machine_learning.m修正
    ・matファイルに直したときに，
    plant_data.input
                     .p
                     .q
                     .v
                     .w
    のような構造体になっていて，pythonで使えない形になっていたのを修正

--- save_data_for_machine_learning.m
+++ save_data_for_machine_learning.m
@@ -1,7 +1,7 @@
 % Data/Exp_data の全てのmatファイルを学習用フォルダData/learning_dataに格納するようにしたい！！
 % 2025.06.02
 
-phase = 'atfl';
+phase = 'tfl';
 
 folder_path = 'Data/Exp_data/'; % 参照したいデータが保存されているフォルダのパス
 learning_folder = 'Data/learning_data/'; % 学習データ保存用フォルダのパス
@@ -10,12 +10,12 @@ matfiles = dir(fullfile(folder_path, '*.mat'));
 file_names = {matfiles.name};
 for i = 1:length(file_names)
     file_name = load([folder_path, file_names{i}]);
-    plant_data = trim_Exp_data(file_name, phase);
-    save(fullfile(learning_folder, file_names{i}), 'plant_data')
+    [input, p, q, v, w] = trim_Exp_data(file_name, phase);
+    save(fullfile(learning_folder, file_names{i}), 'input', 'p', 'q', 'v', 'w')
 end
 
 
-function plant_data = trim_Exp_data(Data, phase)
+function [input, p, q, v, w] = trim_Exp_data(Data, phase)
 % 任意のフェーズをdouble型に変換する関数
     % Data: cell配列になっていてDNN用に整形したいExpの元データ
     % phase: 'f', 'tf', 'atf', 'fl', 'tfl', 'atfl'から選択可能
@@ -63,18 +63,18 @@ function plant_data = trim_Exp_data(Data, phase)
     end
 
     fprintf('selected phase: %s', phase)
-    % plant_data.i = Data.log.target
-    plant_data.input = data.controller.input(:,firstindex:lastindex);
-    plant_data.q = data.estimator.q(:,firstindex:lastindex);
-    plant_data.p = data.estimator.p(:,firstindex:lastindex);
-    plant_data.v = data.estimator.v(:,firstindex:lastindex);
-    plant_data.w = data.estimator.w(:,firstindex:lastindex);
+    % i = Data.log.target
+    input = data.controller.input(:,firstindex:lastindex);
+    q = data.estimator.q(:,firstindex:lastindex);
+    p = data.estimator.p(:,firstindex:lastindex);
+    v = data.estimator.v(:,firstindex:lastindex);
+    w = data.estimator.w(:,firstindex:lastindex);
     
 
 % Square_coloring(Xrange, color, Yrange, Bace, ax) 使えそう
     % 確認用
     time = data.t(firstindex:lastindex);
-    plot(time, plant_data.p(1,:), time, plant_data.p(2,:), time, plant_data.p(3,:))
+    plot(time, p(1,:), time, p(2,:), time, p(3,:))
     hold on
     plot(time, data.reference.p(1,firstindex:lastindex), '--', ...
         time, data.reference.p(2,firstindex:lastindex), '--', ...
@@ -86,4 +86,5 @@ function plant_data = trim_Exp_data(Data, phase)
     grid on
     ax = gca;
     ax.FontSize = 12;
+    hold off
 end
