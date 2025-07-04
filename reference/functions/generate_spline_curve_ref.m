5484f27dadd4b6093e557789491f145d465a66dc
Kudo2026 <g2212040@tcu.ac.jp>
2025-07-04 13:56:59 +0900

    式をgen_ref_for_HLで微分しないならコメント部分はいらなくなる

--- reference/functions/generate_spline_curve_ref.m
+++ reference/functions/generate_spline_curve_ref.m
@@ -49,11 +49,11 @@ ManualSetting = param.ManualSetting;
     end
     t = param.controller_time.t;
     ref_data = way_point_ref(waypoints,order,fshowfig);%補間式の係数など計算
-    information = make_reference(ref_data);
-    function information = make_reference(ref_data)
-        information = @(t) spline_curve(ref_data,t)
-    end
-
+    % information = make_reference(ref_data);
+    % function information = make_reference(ref_data)
+    %     information = @(t) spline_curve(ref_data,t);
+    % end
+    
     function trajectory = spline_curve(ref_data,t)
             %区間ごとの補間式を作成
             t_mod = mod(t,ref_data.period);
@@ -68,8 +68,8 @@ ManualSetting = param.ManualSetting;
                 trajectory = trajectory + interpolation(:,i).*h1.*h2;
             end
     end
-    ref = @(t) information(t);
-    % ref=@(t) spline_curve(ref_data,t);
+    % ref = @(t) information(t);
+    ref=@(t) spline_curve(ref_data,t);
 
     if  exist('ManualSetting','var') %waypointを保存するか選べる
         isSaved = [];
