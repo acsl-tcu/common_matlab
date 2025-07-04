108c3e93e5bed079b7d73f173d855d4252e87645
Kudo2026 <g2212040@tcu.ac.jp>
2025-07-04 14:53:40 +0900

    ・速度，加速度リファレンス追加　・いらない要素削除

--- reference/functions/generate_spline_curve_ref.m
+++ reference/functions/generate_spline_curve_ref.m
@@ -1,6 +1,4 @@
-% function ref = generate_spline_curve_ref(te,filename,order,isManualSetting)
 function ref = generate_spline_curve_ref(param)
-%% この関数でやりたい処理
 %% この関数でやりたい処理
 % isManualSetting = 1のときにここでwaypointを設定する
 % 保存したmatファイルからway_point_refを呼び出す
@@ -10,11 +8,10 @@ function ref = generate_spline_curve_ref(param)
 % order: 何次までのスプラインか(default:5)
 % isManualSetting: 手動で設定するか、読み込むか
 arguments
+    param.point
     param.order
-    param.te
     param.filename
     param.ManualSetting
-    param.controller_time
 end
 
 ManualSetting = param.ManualSetting;
@@ -25,7 +22,7 @@ ManualSetting = param.ManualSetting;
         load(strcat('Data/reference/', filename)); % for exp
         fshowfig = 0; % 読み込んだ時はグラフは描画しない
     else
-        pointN = 8; %waypointの数 default:5, y方向のみの時は7
+        pointN = param.point; %waypointの数 
         dt = 6;%waypoint間の時間
         time =  (0:dt:dt*(pointN-1))';
 
@@ -44,29 +41,35 @@ ManualSetting = param.ManualSetting;
 
         wp = [0, 0, 1;wp_xy, wp_z; 0, 0, 1];
         waypoints = [time, wp];
-        order = param.order;%多項式の次数
+        order = param.order;%多項式の次数※3次までしかできない
         fshowfig = 1;
     end
-    t = param.controller_time.t;
     ref_data = way_point_ref(waypoints,order,fshowfig);%補間式の係数など計算
     % information = make_reference(ref_data);
     % function information = make_reference(ref_data)
     %     information = @(t) spline_curve(ref_data,t);
     % end
     
-    function trajectory = spline_curve(ref_data,t)
+    function ref = spline_curve(ref_data,t)
             %区間ごとの補間式を作成
             t_mod = mod(t,ref_data.period);
             for i=1:ref_data.Sn
-            interpolation(:,i) = ref_data.coefficients.d0(:,:,i)*ref_data.t_powers.d0(t_mod-ref_data.time(i));
+            interpolation_p(:,i) = ref_data.coefficients.d0(:,:,i)*ref_data.t_powers.d0(t_mod-ref_data.time(i));
+            interpolation_v(:,i) = ref_data.coefficients.d1(:,:,i)*ref_data.t_powers.d1(t_mod-ref_data.time(i));
+            interpolation_a(:,i) = ref_data.coefficients.d2(:,:,i)*ref_data.t_powers.d2(t_mod-ref_data.time(i));
             end
             %どの区間か判断
-            trajectory = zeros(3,1); % 3×1のゼロベクトル
+            p_ref = zeros(3,1); % 3×1のゼロベクトル
+            v_ref = zeros(3,1);
+            a_ref = zeros(3,1);
             for i = 1:ref_data.Sn
                 h1 = heaviside(t_mod - ref_data.time(i));
                 h2 = heaviside(ref_data.time(i+1) - t_mod);
-                trajectory = trajectory + interpolation(:,i).*h1.*h2;
+                p_ref = p_ref + interpolation_p(:,i).*h1.*h2;
+                v_ref = v_ref + interpolation_v(:,i).*h1.*h2;
+                a_ref = a_ref + interpolation_a(:,i).*h1.*h2;
             end
+            ref = [p_ref;v_ref;a_ref];
     end
     % ref = @(t) information(t);
     ref=@(t) spline_curve(ref_data,t);
