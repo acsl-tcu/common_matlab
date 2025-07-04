fd6e09b4882f3258cc2b2c635bfdbceb4a3cbddd
1912063 <g2212044@tcu.ac.jp>
2025-07-04 17:34:48 +0900

    onnxファイルを色々実行
    ・発散したり，小さすぎたり…
    →小さすぎると思っているけど，値としては十分なのかも？？

--- mode/SimHL_MEC.m
+++ mode/SimHL_MEC.m
@@ -33,7 +33,7 @@ run("ExpBase");
 agent.cha_allocation.reference = "time_varying";
 
 agent.controller.nominal = HLC(agent,Controller_HL(dt));
-agent.controller.mec = DNNMEC(agent, "epoch_100000_coef_1_0.5_0.5_0.5_0.5.onnx");
+agent.controller.mec = DNNMEC(agent, "epoch_100000.onnx");
 agent.cha_allocation.controller=["nominal","mec"]; % cha_allocationにコントローラー登録
 
 function dfunc(app)
