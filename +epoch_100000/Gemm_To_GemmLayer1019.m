9780d8459ce19259dc13ad158c9f8603b7c7f31c
YoshikiK-tcu <g2112022@tcu.ac.jp>
2025-07-09 10:14:51 +0900

    gen_ref_spline追加

--- +epoch_100000/Gemm_To_GemmLayer1019.m
+++ +epoch_100000/Gemm_To_GemmLayer1019.m
@@ -1,12 +1,12 @@
 classdef Gemm_To_GemmLayer1019 < nnet.layer.Layer & nnet.layer.Formattable
     % A custom layer auto-generated while importing an ONNX network.
-
+    
     %#codegen
     %#ok<*PROPLC>
     %#ok<*NBRAK>
     %#ok<*INUSL>
     %#ok<*VARARG>
-
+    
     properties (Learnable)
         linears_0_bias
         linears_0_weight
@@ -17,11 +17,11 @@ classdef Gemm_To_GemmLayer1019 < nnet.layer.Layer & nnet.layer.Formattable
         linears_3_bias
         linears_3_weight
     end
-
+    
     properties
         ONNXParams         % An ONNXParameters object containing parameters used by this layer.
     end
-
+    
     methods
         function this = Gemm_To_GemmLayer1019(name, onnxParams)
             this.Name = name;
@@ -37,7 +37,7 @@ classdef Gemm_To_GemmLayer1019 < nnet.layer.Layer & nnet.layer.Formattable
             this.linears_3_bias = onnxParams.Learnables.linears_3_bias;
             this.linears_3_weight = onnxParams.Learnables.linears_3_weight;
         end
-
+        
         function [output] = predict(this, input, inputNumDims)
             if isdlarray(input)
                 input = stripdims(input);
@@ -64,7 +64,7 @@ classdef Gemm_To_GemmLayer1019 < nnet.layer.Layer & nnet.layer.Formattable
                 output = extractdata(output);
             end
         end
-
+        
         function [output] = forward(this, input, inputNumDims)
             if isdlarray(input)
                 input = stripdims(input);
@@ -288,9 +288,6 @@ end
 %% dlarray functions implementing ONNX operators:
 
 function [A, B, C, alpha, beta, numDimsY] = prepareGemmArgs(A, B, C, alpha, beta, transA, transB, numDimsC)
-
-%   Copyright 2020 The MathWorks, Inc.
-
 % Prepares arguments for implementing the ONNX Gemm operator
 if transA
     A = A';
@@ -309,9 +306,6 @@ end
 
 function s = appendStructs(varargin)
 % s = appendStructs(s1, s2,...). Assign all fields in s1, s2,... into s.
-
-%   Copyright 2020 The MathWorks, Inc.
-
 if isempty(varargin)
     s = struct;
 else
@@ -327,8 +321,6 @@ end
 end
 
 function checkInputSize(inputShape, expectedShape, inputName)
-
-%   Copyright 2020-2021 The MathWorks, Inc.
 
 if numel(expectedShape)==0
     % The input is a scalar
@@ -346,29 +338,29 @@ elseif numel(expectedShape)==1
     end
 else
     % The input has 2 dimensions or more
-
+    
     % The input dimensions have been reversed; flip them back to compare to the
     % expected ONNX shape.
     inputShape = fliplr(inputShape);
-
+    
     % If the expected shape has fewer dims than the input shape, error.
     if numel(expectedShape) < numel(inputShape)
         expectedSizeStr = strjoin(["[", strjoin(string(expectedShape), ","), "]"], "");
         error(message('nnet_cnn_onnx:onnx:InputHasGreaterNDims', inputName, expectedSizeStr));
     end
-
+    
     % Prepad the input shape with trailing ones up to the number of elements in
     % expectedShape
     inputShape = num2cell([ones(1, numel(expectedShape) - length(inputShape)) inputShape]);
-
+    
     % Find the number of variable size dimensions in the expected shape
     numVariableInputs = sum(cellfun(@(x) isa(x, 'char') || isa(x, 'string'), expectedShape));
-
+    
     % Find the number of input dimensions that are not in the expected shape
     % and cannot be represented by a variable dimension
     nonMatchingInputDims = setdiff(string(inputShape), string(expectedShape));
     numNonMatchingInputDims  = numel(nonMatchingInputDims) - numVariableInputs;
-
+    
     expectedSizeStr = makeSizeString(expectedShape);
     inputSizeStr = makeSizeString(inputShape);
     if numNonMatchingInputDims == 0 && ~iSizesMatch(inputShape, expectedShape)
@@ -408,9 +400,6 @@ end
 end
 function X = makeUnlabeledDlarray(X)
 % Make numeric X into an unlabelled dlarray
-
-%   Copyright 2020-2021 The MathWorks, Inc.
-
 if isa(X, 'dlarray')
     X = stripdims(X);
 elseif isnumeric(X)
@@ -424,9 +413,6 @@ end
 end
 
 function [Vars, NumDims] = packageVariables(params, inputNames, inputValues, inputNumDims)
-
-%   Copyright 2020 The MathWorks, Inc.
-
 % inputNames, inputValues are cell arrays. inputRanks is a numeric vector.
 Vars = appendStructs(params.Learnables, params.Nonlearnables, params.State);
 NumDims = params.NumDimensions;
@@ -438,8 +424,6 @@ end
 end
 
 function X = permuteInputVar(X, userDataPerm, onnxNDims)
-
-%   Copyright 2020-2021 The MathWorks, Inc.
 % Returns reverse-ONNX ordering
 if onnxNDims == 0
     return;
@@ -467,8 +451,6 @@ X = permute(X, perm);
 end
 
 function Y = permuteOutputVar(Y, userDataPerm, onnxNDims)
-
-%   Copyright 2020-2021 The MathWorks, Inc.
 switch onnxNDims
     case 0
         perm = [];
@@ -517,10 +499,7 @@ end
 end
 
 function s = updateStruct(s, t)
-% Set all existing fields in s from fields in t, ignoring extra fields in
-% t.
-%   Copyright 2020 The MathWorks, Inc.
-
+% Set all existing fields in s from fields in t, ignoring extra fields in t.
 for name = transpose(fieldnames(s))
     s.(name{1}) = t.(name{1});
 end
--- mode/ExpHL.m
+++ mode/ExpHL.m
@@ -23,19 +23,27 @@ agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_Eule
 agent.sensor = MOTIVE(agent, Sensor_Motive(1,0, motive));
 agent.input_transform = THRUST2THROTTLE_DRONE(agent,InputTransform_Thrust2Throttle_drone()); % 推力からスロットルに変換
 
-agent.reference.timevarying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",10,"orig",[0;0;1],"size",[1,1,0.2]},"HL"});
-% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_spline",{"point",10,"order",9,"point_dt",5,"ManualSetting",0}});%HLを付けると軌道が微分される
+% agent.reference.timevarying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",10,"orig",[0;0;1],"size",[1,1,0.2]},"HL"});
+agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_spline",{"point",20,"order",9,"point_dt",5,"ManualSetting",0}});%HLを付けると軌道が微分される
 agent.controller = HLC(agent,Controller_HL(dt));
 
 run("ExpBase");
-agent.cha_allocation.reference = "timevarying";
+agent.cha_allocation.reference = "time_varying";
 function post(app)
-app.logger.plot({1, "p", "ers"},"ax",app.UIAxes,"xrange",[app.time.ts,app.time.te]);
-% app.logger.plot({1, "inner_input", ""},"ax",app.UIAxes2,"xrange",[app.time.ts,app.time.te]);
-% app.logger.plot({1, "v", "e"},"ax",app.UIAxes3,"xrange",[app.time.ts,app.time.te]);
-% app.logger.plot({1, "input", ""},"ax",app.UIAxes3,"xrange",[app.time.ts,app.time.te]);
-% app.logger.plot({1, "input", ""},"ax",app.UIAxes5,"xrange",[app.time.ts,app.time.te]);
-% app.logger.plot({1, "inner_input", ""},"ax",app.UIAxes6,"xrange",[app.time.ts,app.time.te]);
+LW = 1.0; % Linewidth 
+FS = 20; % Fontsize
+app.logger.plot({1, "p", "er"},"ax",app.UIAxes,"phase","tfl", "Linewidth",LW, "Fontsize",FS);
+app.logger.plot({1, "q", "e"}, "phase","tfl", "fig_num",2, "Linewidth",LW, "Fontsize",FS);
+app.logger.plot({1, "v", "er"}, "phase","tfl", "fig_num",3, "Linewidth",LW, "Fontsize",FS);
+app.logger.plot({1, "w", "e"}, "phase","tfl", "fig_num",4, "Linewidth",LW, "Fontsize",FS);
+% app.logger.plot({{1, "input", ""}, {1, "controller.result.nominal_input", ""},...
+%     {1, "controller.result.delta_input", ""}}, "phase","tfl","fig_num",5); % inputをまとめて見る
+app.logger.plot({1, "input", ""}, "phase","tfl", "fig_num",6, "Linewidth",LW, "Fontsize",24);
+% app.logger.plot({1, "controller.result.nominal_input", ""}, "phase","tfl", "fig_num",7, "Linewidth",LW, "Fontsize",FS);
+% app.logger.plot({1, "controller.result.delta_input", ""}, "phase","tfl", "fig_num",8, "Linewidth",LW, "Fontsize",FS);
+
+app.logger.plot({1, "p1-p2", "er"}, "phase","tfl", "color", 0, "fig_num",9, "Linewidth",LW, "Fontsize",FS);
+app.logger.plot({1, "p1-p2-p3", "er"}, "phase","tfl", "color", 0, "fig_num",10, "Linewidth",LW, "Fontsize",FS);
 end
 function in_prog(app)
 app.TextArea.Text = "estimator : " + app.agent(1).estimator.result.state.get();
