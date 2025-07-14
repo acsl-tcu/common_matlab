72d36f01e46e5b68637517af61af55ffe5cd5d57
Kudo2026 <g2212040@tcu.ac.jp>
2025-07-14 10:19:11 +0900

    Update mainGUI.m

--- mainGUI.m
+++ mainGUI.m
@@ -22,9 +22,9 @@ ExpBaseMode = ["ExpSuspendedLoad","ExpSuspendedLoadCoop","ExpTestMotiveConnectio
 
 Setting.fDebug = 1; % 1: active : for debug function
 Setting.PInterval = 0.6; % sec : poling interval for emergency stop
-% Setting.mode = SimBaseMode(3); % SimHL
+Setting.mode = SimBaseMode(3); % SimHL
 % Setting.mode = SimBaseMode(7); % SimFHL
-Setting.mode = SimBaseMode(4); % SimMEC_K
+% Setting.mode = SimBaseMode(4); % SimMEC_K
 
 % Setting.mode = ExpBaseMode(4); % ExpHL
 if contains(Setting.mode,"Exp")
