3b07ba6ec2a834d3de582135710e90cdd960645d
Kudo2026 <g2212040@tcu.ac.jp>
2025-07-16 17:25:01 +0900

    クープマンモデル追加

--- sample/Controller_FHLMECK.m
+++ sample/Controller_FHLMECK.m
@@ -13,7 +13,8 @@ Controller.F = blkdiag(Controller.F1,Controller.F2,Controller.F3,Controller.F4);
 
 %% Koopman
 % modeファイルとファイル名をそろえる
-load("koopman.mat",'est'); 
+% load("koopman.mat",'est'); 
+load("koopman_model_first.mat",'est');
 % load("EstimationResult_12state_2_7_Exp_sprine+zsprine+P2Pz_torque_incon_150data_vzからz算出.mat",'est');
 Controller.est = est;
 
