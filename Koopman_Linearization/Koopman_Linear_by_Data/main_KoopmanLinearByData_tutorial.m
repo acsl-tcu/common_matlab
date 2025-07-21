51e6352dfbad7c64632cfb190d822515907b538a
miyachi_acslPC <g2212040@tcu.ac.jp>
2025-07-21 15:14:29 +0900

    Update main_KoopmanLinearByData_tutorial.m

--- Koopman_Linearization/Koopman_Linear_by_Data/main_KoopmanLinearByData_tutorial.m
+++ Koopman_Linearization/Koopman_Linear_by_Data/main_KoopmanLinearByData_tutorial.m
@@ -56,8 +56,6 @@ targetpath=append(nowFolder,'\',FileName);
 
 %% Defining Koopman Operator
 %<使用している観測量>
-% F = @(x) [x;1]; % 状態変数+定数項1
-% F = @quaternions; % 状態+クォータニオンの1乗2乗3乗 オイラー角パラメータ用
 F = @quaternions_all; 
 fprintf('\n選択されている観測量：%s\n',func2str(F))
 
@@ -232,14 +230,5 @@ disp(targetpath)
 % end
 % 
 % fprintf('\n＜ファイルの移動が完了しました＞\n')
-%% 先輩が今まで作られた観測量
 
-% F = @(x) [x;1]; % 状態そのまま
-% F = @quaternionParameter; % クォータニオンを含む13状態の観測量
-% F = @eulerAngleParameter; % 姿勢角をオイラー角モデルの状態方程式からdq/dt部分を抜き出した観測量
-% F = @eulerAngleParameter_withinConst; % eulerAngleParameter+慣性行列を含む部分(dvdt)を含む観測量
-% F = @eulerAngleParameter_InputAndConst; % eulerAngleParameter_withinConst+入力にかかる係数行列の項を含む観測量
-% F = @quaternions; % 状態+クォータニオンの1乗2乗3乗 オイラー角パラメータ用(動作確認済み) <こちらが最新の観測量>
-% F = @quaternions_13state; % 状態+クォータニオンの1乗2乗3乗 クォータニオンパラメータ用
-% F = @eulerAngleParameter_withoutP;
 
