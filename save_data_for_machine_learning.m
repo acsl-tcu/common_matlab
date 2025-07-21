2471e49851ccf815a2c28a8280a69b9bbd8071a9
1912063 <g2212044@tcu.ac.jp>
2025-07-21 18:36:32 +0900

    plot_from_saved_result.m を修正．
    LOGGERのdataメソッドを使うようにしたらめっちゃ簡素化できた！！

--- save_data_for_machine_learning.m
+++ save_data_for_machine_learning.m
@@ -1,10 +1,47 @@
-% Data/Exp_data の全てのmatファイルを学習用フォルダData/learning_dataに格納するようにしたい！！
+%% 2025.07.21
+% このセクションを実行するだけで良い
+% LOGGERのdataメソッドを使用している
+
+phase = 'f';
+
+folder_path = 'Data/Exp_data/2025.07.21_Exp/'; % 参照したいデータが保存されているフォルダのパス
+learning_folder = 'Data/learning_data/'; % 学習データ保存用フォルダのパス
+
+matfiles = dir(fullfile(folder_path, '*.mat'));
+file_names = {matfiles.name};
+variables = ["input", "p", "q", "v", "w"];
+attributes = ["", "e", "e", "e", "e"];
+
+fprintf('Selected phase: %s', phase)
+
+for i=1:length(file_names)
+    file_name = fullfile(folder_path, file_names{i});
+    logger = LOGGER(file_name);
+    for j=1:length(variables)
+        tmp = logger.data(1, variables(j), attributes(j), "phase",phase);
+        switch variables(j)
+            case "input", input = tmp';
+            case "p", p = tmp';
+            case "q", q = tmp';
+            case "v", v = tmp';
+            case "w", w = tmp';
+        end
+    end
+    save(fullfile(learning_folder, file_names{i}), "input", "p", "q", "v", "w")
+end
+
+logger.plot({1,"p","e"}, "fig_num",1, "phase",phase) % for check
+
+
+
+%% Data/Exp_data の全てのmatファイルを学習用フォルダData/learning_dataに格納するようにしたい！！
 % 2025.06.02
 
 phase = 'tfl';
+phase = 'f';
 
 % folder_path = 'Data/Exp_data/'; % 参照したいデータが保存されているフォルダのパス
-folder_path = 'Data/Exp_data/2025.07.09_Exp/'; % 参照したいデータが保存されているフォルダのパス
+folder_path = 'Data/Exp_data/2025.07.21_Exp/'; % 参照したいデータが保存されているフォルダのパス
 learning_folder = 'Data/learning_data/'; % 学習データ保存用フォルダのパス
 
 matfiles = dir(fullfile(folder_path, '*.mat'));
