8812b9f6523693b7e7f4d027c821123d659b47e4
Kudo2026 <g2212040@tcu.ac.jp>
2025-07-31 16:03:59 +0900

    1.5にしないとfで0.5から始まる

--- mode/SimMEC_K.m
+++ mode/SimMEC_K.m
@@ -45,7 +45,7 @@ agent.plant = MODEL_CLASS(agent,plant_model);
 agent.sensor = DIRECT_SENSOR(agent, 0.0); % modeファイル内で回すとき
 agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)),["p", "q"]));
 
-agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[1,1,0.5]},"HL"});
+agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1.5],"size",[1,1,0.5]},"HL"});
 % agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_circle",{"freq",5,"init",[0;0;1],"radius",1.0},"HL"});
 % agent.reference = TIME_VARYING_REFERENCE(agent,{"gen_ref_p2p",{"freq",5,"init",[0;0;1],"radius",1.0},"HL"});
 run("ExpBase");
