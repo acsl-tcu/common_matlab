d34d734e8c43ab7b7c20b940dff1c0cb329e5c0b
Atanaka <166685166+Atanaka-tcu@users.noreply.github.com>
2025-09-16 11:17:41 +0900

    HLC_SUSPENDED_LOADが二重になっていたので追加したものを消去

--- mainGUI.m
+++ mainGUI.m
@@ -22,8 +22,10 @@ ExpBaseMode = ["ExpSuspendedLoad","ExpSuspendedLoadCoop","ExpTestMotiveConnectio
 
 Setting.fDebug = 1; % 1: active : for debug function
 Setting.PInterval = 0.6; % sec : poling interval for emergency stop
-Setting.mode = SimBaseMode(2); % SimHL
+Setting.mode = SimBaseMode(1); % SimHL
 % Setting.mode = SimBaseMode(7); % SimFHL
+% Setting.mode = SimBaseMode(2); % SimCoop
+
 
 % Setting.mode = ExpBaseMode(1); % ExpHL
 if contains(Setting.mode,"Exp")
