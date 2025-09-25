a33ee87068fff104510054267c2dddff96cbb54f
Kudo2026 <g2212040@tcu.ac.jp>
2025-09-25 16:30:03 +0900

    回転行列の一部計算方法変更

--- kalman_decoupling.m
+++ kalman_decoupling.m
@@ -1,8 +1,8 @@
 clear;
 clc;
 % load("without_w1.mat");
-load("koopman_model_first.mat",'est');
-% load("koopman_common_z_.mat");
+% load("koopman_model_first.mat",'est');
+load("koopman_common_z_.mat");
 % load("EstimationResult_12state_2_7_Exp_sprine+zsprine+P2Pz_torque_incon_150data_vzからz算出.mat",'est');
 
 % 可制御性行列
