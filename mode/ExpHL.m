f4e1edd14fdb3e2641196a04b2ef5ae0d9fcc2a0
YosukeKoseki <g2212044@tcu.ac.jp>
2025-10-01 15:13:42 +0900

    刻み時間プロット修正

--- mode/ExpHL.m
+++ mode/ExpHL.m
@@ -17,8 +17,7 @@ initial_state.w = [0; 0; 0];
 
 agent = DRONE;
 % agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "udp", )[1, 252]));
-agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial", "COM5"));
-% ヤマト->COM3　イセ->COM4  ムサシ->COM6  ナガト->COM5 
+agent.plant = DRONE_EXP_MODEL(agent,Model_Drone_Exp(dt, initial_state, "serial", "COM4"));
 agent.parameter = DRONE_PARAM("DIATONE");
 agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)),["p", "q"]));
 agent.sensor = MOTIVE(agent, Sensor_Motive(1,0, motive));
@@ -31,8 +30,8 @@ center = [0;0;takeoff_zd];
 % agent.reference.time_varying
 % agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",10,"orig",[1;-1;1.5],"size",[0,0,0]},"HL"}); % hovering
 % agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",10,"orig",[0;0;1],"size",[1,1,0]},"HL"}); % circle
-agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",10,"orig",[0;0;1],"size",[1,1,0.2]},"HL"}); % saddle
-% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_spline",{"point",20,"order",9,"point_dt",2.5,"ManualSetting",0,"check",1}}); % spline
+% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",10,"orig",[0;0;1],"size",[1,1,0.2]},"HL"}); % saddle
+agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_spline",{"point",20,"order",9,"point_dt",2.5,"ManualSetting",0,"check",1}}); % spline
 % agent.reference.time_varying = MY_POINT_REFERENCE(agent, {struct("f", center, "g", [-1;-1;takeoff_zd], "h",[0;0;takeoff_zd+0.5]), 15}); % P2P
 agent.cha_allocation.reference = "time_varying";
 function post(app)
@@ -54,19 +53,23 @@ app.logger.plot({1, "p1-p2", "er"}, "phase","tfl", "color", 0, "fig_num",10, "Li
 app.logger.plot({1, "p1-p2-p3", "er"}, "phase","tfl", "color", 0, "fig_num",11, "Linewidth",LW, "Fontsize",FS);
 
 
-% % 刻み時間描画
-% t0id = find(app.logger.Data.phase==97,1,'last')+1;
-% teid = find(app.logger.Data.phase==0,1,'first')-1;
-% dt = diff(app.logger.Data.t(t0id:teid));
-% t = app.logger.Data.t(t0id:teid-1);
-% figure(100)
-% [t,dt];
-% plot(t,dt, Linewidth=LW);
-% hold on
-% yline(0.025,"LineWidth",LW)
-% hold off
-% grid on
-% legend("dt","25 ms")
+% 刻み時間描画
+t0id = find(app.logger.Data.phase==97,1,'last')+1;
+teid = find(app.logger.Data.phase==0,1,'first')-1;
+dt = diff(app.logger.Data.t(t0id:teid));
+t = app.logger.Data.t(t0id:teid-1);
+figure(100)
+ax = gca;
+[t,dt];
+plot(t,dt, Linewidth=LW);
+hold on
+yline(0.025,"LineWidth",LW)
+hold off
+grid on
+legend("dt","25 ms")
+set(ax.XAxis, fontsize=FS-2)
+set(ax.YAxis, fontsize=FS-2)
+set(ax.Legend, 'FontSize',FS-4);
 
 end
 function in_prog(app)
