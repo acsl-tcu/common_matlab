1974c6203332b25aa4a40c049e05b29937543f18
Yosuke KOSEKI <g2212044@tcu.ac.jp>
2025-10-03 16:35:32 +0900

    SimHLでeuler_parameter_thrust_force_physical_parameter_modelを使ってみた
    ・色々変えてもシミュレーション開始して1秒経たないうちに発散…なんで？？

--- mode/SimHL.m
+++ mode/SimHL.m
@@ -11,7 +11,7 @@ end
 %%
 ts = 0; % initial time
 dt = 0.025; % sampling period
-te = 200; % terminal time
+te = 100; % terminal time
 time = TIME(ts,dt,te); % instance of time class
 in_prog_func = @(app) dfunc(app); % in progress plot
 post_func = @(app) dfunc(app); % function working at the "draw button" pushed.
@@ -27,24 +27,28 @@ agent = DRONE;
 agent.parameter = DRONE_PARAM("DIATONE");
 % プラントモデル定義 ================================================================================================================================
 % agent.plant = MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1));
-agent.plant = MODEL_CLASS(agent,Model_Quat13(dt, initial_state, 1)); % Model_Quat13
+plant = Model_Quat13(dt, initial_state, 1);
+plant.param.method = "euler_parameter_thrust_force_physical_parameter_model";
+plant.param.dim = [13,4,18];
+agent.plant = MODEL_CLASS(agent, plant); % Model_Quat13
 
 % デフォルト物理パラメータ(DRONE_PARAM.m準拠: 2025/07/07時点)
 % 1:mass=0.75  |  2,3:Lx,y=0.16  |  4,5: lx,y=0.08  |  6,7,8: jx,y,z=0.06  |  9: gravity=9.81
 % 10,11,12,13: km(各ロータ定数)=0.0301  |  14,15,16,17: k(推力定数)=8.0e-6  |  18: rotor_r=0.0392
 
 % ↓パラメータの上書き モデル誤差をプラントに与える
-agent.plant.param(1) = 0.7875; % ５％増->0.7875, ５％減->0.7125
-agent.plant.param(6) = 0.2; % 0.18<jx,jy<0.22ぐらいが良き frequency=5の時
-agent.plant.param(7) = 0.2; % 同上
+% agent.plant.param(1) = 0.7875; % ５％増->0.7875, ５％減->0.7125
+% agent.plant.param(6) = 0.2; % 0.18<jx,jy<0.22ぐらいが良き frequency=5の時
+% agent.plant.param(7) = 0.2; % 同上
+agent.plant.param(10:13) = [0.003, 0.003, 0.003, 0.003];
 %===================================================================================================================================================
 agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)),["p", "q"]));
 agent.sensor = MOTIVE(agent, Sensor_Motive(1,0, motive));
 agent.controller = HLC(agent,Controller_HL(dt));
-% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[1,1,0]},"HL"}); % circle
 % agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[0,0,0]},"HL"}); % hovering
-% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[1,1,0.2]},"HL"}); % saddle
-agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_spline",{"point",20,"order",9,"point_dt",5,"ManualSetting",0,"check",1}}); % random 9th spline
+% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[1,1,0]},"HL"}); % circle
+agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",5,"orig",[0;0;1],"size",[1,1,0.2]},"HL"}); % saddle
+% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_spline",{"point",20,"order",9,"point_dt",5,"ManualSetting",0,"check",1}}); % random 9th spline
 run("ExpBase");
 
 agent.cha_allocation.reference = "time_varying";
@@ -54,7 +58,7 @@ function dfunc(app)
 LW = 1.5; % Linewidth 
 FS = 20; % Fontsize
 phase = "tfl";
-phase = "f";
+% phase = "f";
 app.logger.plot({1, "p", "er"},"ax",app.UIAxes,"phase",phase, "Linewidth",LW, "Fontsize",FS);
 app.logger.plot({1, "p", "er"}, "phase",phase, "fig_num",1, "Linewidth",LW, "Fontsize",FS);
 app.logger.plot({1, "q", "e"}, "phase",phase, "fig_num",2, "Linewidth",LW, "Fontsize",FS);
