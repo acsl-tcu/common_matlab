f1410d4ae733c57487445cf74a114648abc44a57
YosukeKoseki <g2212044@tcu.ac.jp>
2025-10-09 17:40:27 +0900

    iikanji

--- mode/ExpMPC_Koopman_kyo.m
+++ mode/ExpMPC_Koopman_kyo.m
@@ -7,7 +7,7 @@ in_prog_func = @(app) in_prog(app);
 post_func = @(app) post(app);
 logger = LOGGER(1, size(ts:dt:te, 2), 1, [],[]);
 mmatflag = 0;
-model_file = '2025-07-30_exp_koseki_code00_randompp';
+model_file = '2025-10-08_exp_ob25_1_code00_randompp';
 motive = Connector_Natnet('192.168.100.4'); % connect to Motive　
 motive.getData([], []); % get data from Motive
 rigid_ids = 1; % rigid-body number on Motive
@@ -26,8 +26,11 @@ agent.input_transform = THRUST2THROTTLE_DRONE(agent,InputTransform_Thrust2Thrott
 % agent.input_transform.param.pitch_offset = 510;
 % agent.input_transform.param.roll_offset = 490;
 % agent.reference.bezier = BEZIER_REFERENCE(agent,{[0,0,0.6]},time);
- % agent.reference.time_var= TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",20,"orig",[0;0;0.6],"size",[0,0,0]},"HL"});%{"Case_study_trajectory",{[0,0,0.6]},"HL"});
-agent.reference.time_var= TIME_VARYING_REFERENCE(agent,{"gen_ref_spline",{"point",10,"order",9,"point_dt",5,"ManualSetting",0,"check",1}});
+ agent.reference.time_var= TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",20,"orig",[0;0;0.6],"size",[0,0,0]},"HL"});%{"Case_study_trajectory",{[0,0,0.6]},"HL"});
+% agent.reference.time_var= TIME_VARYING_REFERENCE(agent,{"gen_ref_spline",{"point",10,"order",9,"point_dt",5,"ManualSetting",0,"check",1}});
+% agent.reference.time_varying = MY_POINT_REFERENCE(agent, {struct("f", center, "g", [1;0;takeoff_zd], "h",center, "j",[0;1;takeoff_zd], "k",center, "z",[0;0;takeoff_zd-0.5], "x",center...
+                                                                % , "c",[-1;-1;takeoff_zd], "v",center, "b",[1;-1;takeoff_zd+0.5], "n",center), 7.5}); % P2P
+% agent.reference.time_varying = MY_POINT_REFERENCE(agent, {struct("f", [0,0,0.6], "g", [-1;-1;0.6], "h",[0;0;1.1]), 15}); % P2P
 %2つのコントローラの設定---------------------------------------------------------------------------------------------------
 agent.controller.hlc = HLC(agent,Controller_HL(dt));
 agent.controller.kmpc = MPC_CONTROLLER_KMC_kyo_guiexperiment(agent,Controller_MPC_KMC_kyo(dt,model_file,agent)); %最適化手法：QP
--- sample/Controller_MPC_KMC_kyo.m
+++ sample/Controller_MPC_KMC_kyo.m
@@ -34,12 +34,19 @@ function Controller = Controller_MPC_KMC_kyo(dt, model_file, agent)
     %-- 観測量の選択
     [Controller.F, Controller.code] = select_observable(model_file);
    
-    %% 
-    Controller.weight.P = 1*diag([100;300;500]);    % 位置　10,20刻み  20;1;30
+    %% sim用　重み
+    % Controller.weight.P = 1*diag([100;300;500]);    % 位置　10,20刻み  20;1;30
+    % Controller.weight.Q = 1e3*diag([1;1;1]);    % 速度  10,20刻み  30;20;10
+    % Controller.weight.V = diag([100;100;100]); % 15良い気がする
+    % Controller.weight.W = diag([1;1;1]);  % 姿勢角，角速度　1,2刻み 
+    % Controller.weight.R = diag([100; 100; 100; 100]); % 入力
+    % Controller.weight.RP = 0*diag([100; 100; 100; 100]);  % 1ステップ前の入力との差    0*(無効化)
+    %%　実験用　重み
+    Controller.weight.P = 1.3*diag([300;300;500]);    % 位置　10,20刻み  20;1;30
     Controller.weight.Q = 1e3*diag([1;1;1]);    % 速度  10,20刻み  30;20;10
     Controller.weight.V = diag([100;100;100]); % 15良い気がする
     Controller.weight.W = diag([1;1;1]);  % 姿勢角，角速度　1,2刻み 
-    Controller.weight.R = diag([100; 100; 100; 100]); % 入力
+    Controller.weight.R = diag([100; 150; 150; 100]); % 入力
     Controller.weight.RP = 0*diag([100; 100; 100; 100]);  % 1ステップ前の入力との差    0*(無効化)
     %%　2025-07-30_exp_koseki_code00_randompp　用重み
     % Controller.weight.P = diag([500;500;200]);    % 位置　10,20刻み  20;1;30
