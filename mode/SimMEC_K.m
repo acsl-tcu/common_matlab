375e7d87db05f41c6c9e7a466403db8293ccfbd7
Kudo2026 <g2212040@tcu.ac.jp>
2025-10-23 17:08:12 +0900

    質量0.9だと発散

--- mode/SimMEC_K.m
+++ mode/SimMEC_K.m
@@ -31,9 +31,10 @@ plant_model = Model_EulerAngle(dt, initial_state, 1);
 
 % ↓パラメータの上書き モデル誤差をプラントに与える
 % plant_model.param.param(1) = 0.7875; % ５％減->0.7125 ５％増->0.7875
+plant_model.param.param(1) = 0.9;
 % plant_model.param.param(1) = 0.6; % ５％減->0.7125 ５％増->0.7875
-plant_model.param.param(6) = 0.19; % 0.18<jx,jy<0.22ぐらいが良き
-plant_model.param.param(7) = 0.19; % 
+% plant_model.param.param(6) = 0.18; % 0.18<jx,jy<0.22ぐらいが良き
+% plant_model.param.param(7) = 0.18; % 
 % plant_model.param.param(8) = 0.6; % 0.18 < jzぐらいが良き
 % plant_model.param.param(10) = 0.6; % ５％減->0.028595
 % plant_model.param.param(13) = 0.3;
@@ -45,9 +46,9 @@ agent.plant = MODEL_CLASS(agent,plant_model);
 agent.sensor = DIRECT_SENSOR(agent, 0.0); % modeファイル内で回すとき
 agent.estimator = EKF(agent, Estimator_EKF(agent,dt,MODEL_CLASS(agent,Model_EulerAngle(dt, initial_state, 1)),["p", "q"]));
 
-% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",10,"orig",[0;0;1.3],"size",[1,1,0]},"HL"});
-agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_circle",{"freq",5,"init",[0;0;1],"radius",1.0},"HL"});
-% agent.reference.time_varying = MY_POINT_REFERENCE(agent, {struct("f", [0;0;1.3], "g", [0.1;0.1;1.3], "h",[0;0;1.3]), 15}); % P2P
+agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",10,"orig",[0;0;1.0],"size",[1,1,0.3]},"HL"});
+% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_circle",{"freq",5,"init",[0;0;1],"radius",1.0},"HL"});
+% agent.reference.time_varying = MY_POINT_REFERENCE(agent, {struct("f", [0;0;1], "g", [1;1;1], "h",[0;0;1]), 15}); % P2P
 run("ExpBase");
 agent.cha_allocation.reference = "time_varying";
 
@@ -66,8 +67,8 @@ app.logger.plot({1, "v", "er"},"xrange",[app.time.ts,app.time.te],"fig_num",3);
 app.logger.plot({1, "controller.result.u_nominal", ""}, "xrange",[app.time.ts,app.time.te],"fig_num",6);
 app.logger.plot({1, "controller.result.delta_u", ""}, "xrange",[app.time.ts,app.time.te],"fig_num",7);
 
-app.logger.plot({1, "p1-p2", "er"},"color", 0,"fig_num",8);
-% app.logger.plot({1, "p1-p2-p3", "er"},"fig_num",9);
+% app.logger.plot({1, "p1-p2", "er"},"color", 0,"fig_num",8);
+app.logger.plot({1, "p1-p2-p3", "er"},"color",0,"fig_num",9);
 % % 刻み時間描画
  t0id = find(app.logger.Data.phase==97,1,'last')+1;
 teid = find(app.logger.Data.phase==0,1,'first')-1;
