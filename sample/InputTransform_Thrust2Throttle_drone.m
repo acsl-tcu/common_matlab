83c62c19a8d3b699b4c2b4266f1fe749d60246d7
KokiIshii <g2212008@tcu.ac.jp>
2025-11-04 11:29:18 +0900

    入力変換ゲイン取得２

--- sample/InputTransform_Thrust2Throttle_drone.m
+++ sample/InputTransform_Thrust2Throttle_drone.m
@@ -7,7 +7,10 @@ function u_trans_param = InputTransform_Thrust2Throttle_drone(varargin)
     u_trans_param.th_offset_tl = 335;         % offset 3s[1021] 4s[900]　発掘[926]
     u_trans_param.gain = [400;400;400;40];
     u_trans_param.th_offset = 335;
-
+    % 手動切替: 1 = オフセット探索, 2 = ゲイン探索, 0 = autotune off
+    % 飛行前にここを変更してください（または呼び出し側で上書き）
+    u_trans_param.mode = 1;
+    
     % u_trans_param.gain_SuspendedLoad =[500;500;500;100]; % gain : [roll pitch yaw throttle]' %不明[850;850;600;600] 4s[700;700;600;400] 複数機[700;700;600;200] 発掘[800;800;800;400]
     % u_trans_param.th_offset_SuspendedLoad = 450;         % offset 3s[1021] 4s[900]　発掘[926]
 end
