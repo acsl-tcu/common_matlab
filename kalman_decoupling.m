d0be9f98a4396037ec33cc4996aec5416cba2f20
Kudo2026 <g2212040@tcu.ac.jp>
2025-12-02 16:17:39 +0900

    直感的に重みづけできるように修正

--- kalman_decoupling.m
+++ kalman_decoupling.m
@@ -81,25 +81,32 @@ if abs(det(T_inv)) < tol
 end
 
 
+
+
+%重みづけ
+Q = diag([ones(1,size(est.A,1))]);
+
+%変換
 T = inv(T_inv);
 F = T_inv\est.A*T_inv;
 G = T_inv\est.B;
 H = est.C*T_inv;
+I = T_inv\Q*T_inv;
 U_uc = T(:,k+1:end);
 contrib = abs(U_uc);
 score = sum(contrib,2);
 
-
 %可制御部分抜き出し
 Ac = F(1:k, 1:k);
 Bc = G(1:k, :);
+Qc = I(1:k,1:k);
 %不可制御部分抜き出し
 Acbar = F(k+1:end,k+1:end);
 Bcbar = G(k+1:end,:);
 
 % Qc = diag([5,5,5,0.1,0.1,0.1,ones(1,size(Ac,1)-6)]);
-Qc = diag([ones(1,size(Ac,1))]);
-Rc = 0.5*eye(4);
+% Qc = diag([ones(1,size(Ac,1))]);
+Rc = 1*eye(4);
 Kc = dlqr(Ac, Bc, Qc, Rc);
 K_all = [Kc,zeros(size(est.B,2),(size(est.A,1)-k))];
 K_full = K_all/T_inv;
