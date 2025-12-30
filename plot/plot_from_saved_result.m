5ffec41c50924bf00261b6f9b62a8166e5f6079e
Kudo2026 <g2212040@tcu.ac.jp>
2025-12-31 00:05:33 +0900

    Update plot_from_saved_result.m

--- plot/plot_from_saved_result.m
+++ plot/plot_from_saved_result.m
@@ -52,8 +52,9 @@ settings.fcolor = 1; % default=1 -> フェーズごとの背景色あり
 %%%%%%%%%%%%%%%%%%%%%%%% chose target %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % settings.target = ["p", "controller.result.delta_u"]
 % settings.target = ["p", "q", "v", "w", "input", "controller.result.delta_u"];
+settings.target = ["p", "q", "v", "w", "input", "p1-p2-p3"];
 % settings.target = ["p", "v", "input"];
-settings.target = ["p", "p1-p2-p3"];
+% settings.target = ["p", "p1-p2-p3"];
 % プロットしたいグラフの情報                                          %
 % p: position    q: angle    v: velocity    w: angular velocity     %
 % input: controller input                                           %
@@ -96,6 +97,10 @@ for i=1:length(settings.target)
             tmp(2:3) = [];
         case "input"
             ylabel = "Controller input [N]";
+            tmp = settings.attribute;
+            tmp(2:3) = [];
+        case "controller.result.delta_u"
+            ylabel = "Controller mec input [N]";
             tmp = settings.attribute;
             tmp(2:3) = [];
         case "p1-p2"
