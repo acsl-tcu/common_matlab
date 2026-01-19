c64a0e111090de365290191456c1829f299a7e8c
YosukeKoseki <g2212044@tcu.ac.jp>
2026-01-19 15:27:13 +0900

    サーボ，別軌道実験

--- controller/FUNCTIONAL_HLC_SERVO.m
+++ controller/FUNCTIONAL_HLC_SERVO.m
@@ -71,6 +71,7 @@ classdef FUNCTIONAL_HLC_SERVO < handle
           % max,min are applied for the safty
             obj.result.input = [max(0,min(10,tmp(1)));max(-1,min(1,tmp(2)));max(-1,min(1,tmp(3)));max(-1,min(1,tmp(4)))];
             obj.result.z= obj.z;
+            obj.result.u_nominal = obj.result.input;%do_propでmecのresult.inpuなどに上書きされるから，unほしいならこの行必要
             result = obj.result;
         end
         function show(obj)
--- mode/ExpMEC_K.m
+++ mode/ExpMEC_K.m
@@ -24,11 +24,11 @@ agent.sensor = MOTIVE(agent, Sensor_Motive(1,0, motive));
 agent.input_transform = THRUST2THROTTLE_DRONE(agent,InputTransform_Thrust2Throttle_drone()); % 推力からスロットルに変換
 % agent.input_transform.param.pitch_offset = 535;
 
-% agent.reference.timevarying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",10,"orig",[0;0;1.3],"size",[1,1,0]},"HL"});
-% agent.reference.timevarying = TIME_VARYING_REFERENCE(agent,{"gen_ref_spline",{"point",15,"order",9,"check",1,"point_dt",5,"ManualSetting",0}});%HLを付けると軌道が微分される
-% agent.reference.timevarying = TIME_VARYING_REFERENCE(agent,{"gen_ref_circle",{"freq",10,"init",[0;0;1.3],"radius",1.0},"HL"});
-% agent.reference.timevarying = MY_POINT_REFERENCE(agent, {struct("f", [0;0;1.3], "g", [0.3;0.3;1.3], "h",[0;0;1.3]), 15}); % P2P
-agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_lemniscate",{"freq",10,"orig",[0;0;1],"radius",1.0},"HL"});
+% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_saddle",{"freq",10,"orig",[0;0;1.3],"size",[1,1,0]},"HL"});
+% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_spline",{"point",15,"order",9,"check",1,"point_dt",5,"ManualSetting",0}});%HLを付けると軌道が微分される
+agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_circle",{"freq",10,"orig",[0;0;1],"radius",1.0},"HL"});
+% agent.reference.time_varying = MY_POINT_REFERENCE(agent, {struct("f", [0;0;1.3], "g", [0.3;0.3;1.3], "h",[0;0;1.3]), 15}); % P2P
+% agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_lemniscate",{"freq",10,"orig",[0;0;1],"radius",1.0},"HL"});
 % agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_flower",{"freq",15,"orig",[0;0;1],"radius",1.0},"HL"});
 % agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_lemniscate_3D",{"freq",10,"orig",[0;0;1],"size",[1,0.5],"phase",[0,0]},"HL"});
 % agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_star",{"freq",15,"orig",[0;0;1],"radius",1.0},"HL"});
@@ -37,7 +37,7 @@ agent.reference.time_varying = TIME_VARYING_REFERENCE(agent,{"gen_ref_lemniscate
 
 
 run("ExpBase");
-agent.cha_allocation.reference = "timevarying";
+agent.cha_allocation.reference = "time_varying";
 % agent.controller.nominal=HLC(agent,Controller_HL(dt));
 agent.controller.nominal = FUNCTIONAL_HLC_SERVO(agent,Controller_FHL_Servo(dt));
 agent.controller.mec=MECKC(agent,Controller_HL(dt));
