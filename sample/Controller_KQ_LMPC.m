3be29fdd94ff4983ec7ce529768300ed7499bbc3
YosukeKoseki <g2212044@tcu.ac.jp>
2026-01-23 16:20:47 +0900

    exp weight update

--- sample/Controller_KQ_LMPC.m
+++ sample/Controller_KQ_LMPC.m
@@ -37,7 +37,7 @@ function Controller = Controller_KQ_LMPC(dt, agent)
     % %-- 観測量の選択
     % [Controller.F, Controller.code] = select_observable(model_file);
    
-    %% sim用　重み
+    %% 実験用　重み
 
      Controller.weight.P = 0.5*diag([1000;1000;1200]);    % 位置　10,20刻み  20;1;30
     Controller.weight.Q = 0.1*diag([300;300;500]);    % 姿勢角15良い気がする
@@ -45,7 +45,7 @@ function Controller = Controller_KQ_LMPC(dt, agent)
     Controller.weight.W = 0.1*diag([200;200;200]);  %角速度　1,2刻み 
     Controller.weight.R = 1*diag([0.8; 0.2; 0.1; 0.1]); % 入力
     Controller.weight.RP =1*diag([1; 0.1; 0.1; 0.1]);  % 1ステップ前の入力との差    0*(無効化)
-    %%　実験用　重み
+    %%　sim用　重み
     % Controller.weight.P = 0.7*diag([1000;1000;1000]);    % 位置　10,20刻み  20;1;30
     % Controller.weight.Q = 0.1*diag([300;300;500]);    % 姿勢角15良い気がする
     % Controller.weight.V = 1*diag([500;500;500]);% 速度  10,20刻み  30;20;10
