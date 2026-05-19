caa00f9378e4a7461788bceffab53216e00198f6
YosukeKoseki <2212044y.koseki@gmail.com>
2026-05-19 12:41:01 +0900

    2025/05/19 SimHL_MEC追加（issueあり）
    ・MODEL_CLASS使用時にobj.paramが消える問題

---
+++ controller/NNMEC.m
@@ -0,0 +1,183 @@
+classdef NNMEC < handle
+    %NNMEC
+    %   クアッドコプター用モデル誤差補償器(MEC)のプログラム
+    %   ニューラルネットワーク(NN)で補償器を設計
+    %   [Inputs]
+    %    self: ドローンのagent
+    %    NN_model_filename = "NNMEC.onnx": インポートするonnxファイルの名前
+    %    "NN12": 次元数(12,21,24)，"Euler":状態更新手法("Euler", "RK4")がファイル名に必要
+    
+    %   2025/07 作成者:B4小関      学番:2212044
+    %   最終更新：2026/04/21
+    
+    properties
+        self
+        result
+        physical_param
+        parameter_name = ["mass", "Lx", "Ly", "lx", "ly", "jx", "jy", "jz", "gravity", "km1", "km2", "km3", "km4", "k1", "k2", "k3", "k4"];
+        agent
+        NN_model_filename   % 読み込みたいONNXモデルのファイル名
+        NNMEC_model         % コード内でのモデル名
+        dx_func             % 状態方程式の関数ハンドル
+        state_renew_func    % 学習時の状態更新手法に合わせるための関数ハンドル
+        gen_data_func       % 入力の次元数に合わせたデータ生成関数ハンドル
+        x_pre               % 前時刻の状態
+        pre_input           % 前時刻の制御入力
+        thrust_lim = 5      % 補償推力入力ΔT の制限値
+        tau_lim = 0.5       % 補償トルク入力Δτ の制限絶対値
+        LPF                 % Low Pass Filterクラス
+    end
+    
+    methods
+        function obj = NNMEC(self, NN_model_filename)
+            % インスタンス
+            obj.self = self;
+            obj.physical_param = self.parameter.get(obj.parameter_name);
+            obj.dx_func = @roll_pitch_yaw_thrust_torque_physical_parameter_model;
+
+            %-%-%-% NN model import & define %-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%
+            obj.NN_model_filename = NN_model_filename;
+            if ~exist("controller\NN_MODEL", "dir")
+                mkdir("controller\NN_MODEL")
+            elseif isempty(dir("controller\NN_MODEL\*.onnx"))
+                error("ACSL: Do not exist <onnx> file in controller\NN_MODEL. ")
+            end
+            NN_model = importNetworkFromONNX("\NN_MODEL\"+obj.NN_model_filename,... % ONNXファイルインポート
+                                                "InputDataFormats", "BC", ... % 入力層定義
+                                                "OutputDataFormats", "BC");   % 出力層定義 "BC" -> [バッチサイズ, 特徴量]の意味
+            
+            % NNに続く数字を抽出 (例: "NN24" -> "24")
+            tokens = regexp(NN_model_filename, 'NN(\d+)', 'tokens');
+            if ~isempty(tokens)
+                model_num = tokens{1}{1}; % 文字列としての数字を取得
+                switch model_num
+                    case '21'
+                        dim = 21;
+                        obj.gen_data_func = @(x_p,x_n) [x_p(1:3)-x_n(1:3); x_p(4:end); x_n(4:end)];
+                    case '12'
+                        dim = 12;
+                        obj.gen_data_func = @(x_p,x_n) x_p - x_n;
+                    case '24'
+                        dim = 24;
+                        obj.gen_data_func = @(x_p,x_n) [x_p; x_n];
+                    otherwise
+                        % 例外処理
+                        error('未知のモデル次元です: %s', model_num);
+                end
+            else % NNの記述がない場合のデフォルト
+                dim = 24;
+                obj.gen_data_func = @(x_p,x_n) [x_p; x_n];
+            end
+            dummyInput = dlarray(randn(dim,1,'single'), 'CB'); % 初期化のためのdummy入力
+            obj.NNMEC_model = initialize(NN_model, dummyInput); % モデルの初期化
+
+            if contains(NN_model_filename, 'Euler') % 状態更新手法を動的に変更
+                obj.state_renew_func = @(x_pre, pre_input, dt) obj.Euler(x_pre, pre_input, dt);
+            elseif contains(NN_model_filename, 'RK4')
+                obj.state_renew_func = @(x_pre, pre_input, dt) obj.RK4(x_pre, pre_input, dt);
+            end
+            %-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%
+
+            fc = 1.5; % LPFのカットオフ周波数
+            Ts = 0.025; % サンプリング周波数
+            obj.LPF = LowPassFilter(fc, Ts);
+            
+            % データ関連の初期化
+            obj.result.nominal_p = zeros(3,1);
+            obj.result.nominal_q = zeros(3,1);
+            obj.result.nominal_v = zeros(3,1);
+            obj.result.nominal_w = zeros(3,1);
+            est_name = self.estimator.name;
+            input_dim = self.estimator.(est_name).model.dim(2);
+            obj.result.nominal_input = zeros(input_dim,1);
+            obj.result.delta_input = zeros(input_dim,1);
+            obj.result.input = zeros(input_dim,1);
+            obj.x_pre = self.estimator.result.state.get;
+            obj.pre_input = zeros(input_dim,1);
+            fprintf('Model file name: %s\n', obj.NN_model_filename);
+        end
+        
+        function result = do(obj, varargin)
+            %-%-%-% ノミナル状態更新 ※状態更新の手法は学習時のものと合わせる %-%-%-%
+            if isfield(varargin{3}.Data.agent, "controller") && isfield(varargin{3}.Data.agent, "estimator")... % ループの最初はLoggingされていなくて，参照できないのを回避
+            && length(varargin{3}.Data.agent.estimator.result)>=2
+                obj.pre_input = varargin{3}.Data.agent.controller.result{end}.input; % LOGGERから前時刻の入力を取得
+                obj.x_pre = varargin{3}.Data.agent.estimator.result{end}.state.get; % LOGGERから前時刻の状態を取得
+            end
+            dt = varargin{1}.dt;
+            x_nominal = obj.state_renew_func(obj.x_pre, obj.pre_input, dt);
+            %-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%
+            obj.result.nominal_p = x_nominal(1:3);
+            obj.result.nominal_q = x_nominal(4:6);
+            obj.result.nominal_v = x_nominal(7:9);
+            obj.result.nominal_w = x_nominal(10:12);
+
+            % プラント値取得
+            x_plant = obj.self.estimator.result.state.get; % 現時刻の推定値
+            % -> size = 12*1, contents = [p; q; v; w];
+
+            % NNへの入力データ
+            data = obj.gen_data_func(x_plant, x_nominal);
+            obj.result.delta_input = -1*double(predict(obj.NNMEC_model, data'))'; % predict関数での推論
+            if abs(obj.result.delta_input(1))>obj.thrust_lim, obj.result.delta_input(1) = 0; end % NN関係　閾値での入力制限
+            if abs(obj.result.delta_input(2))>obj.tau_lim, obj.result.delta_input(2) = 0; end
+            if abs(obj.result.delta_input(3))>obj.tau_lim, obj.result.delta_input(3) = 0; end
+            if abs(obj.result.delta_input(4))>obj.tau_lim, obj.result.delta_input(4) = 0; end
+
+            % obj.result.delta_input = [0;0;0;0]; % 補償入力を無くしてNN-MECを入れない
+            % obj.result.delta_input = obj.LPF.update(obj.result.delta_input); % 補償入力delta_uにLPFを掛ける
+
+            obj.result.nominal_input = varargin{5}.controller.nominal.result.input; % ノミナル入力を保存
+            obj.result.input = obj.result.nominal_input + obj.result.delta_input;
+            result = obj.result;
+        end
+
+
+
+        function x_plus = Euler(obj, x_pre, pre_input, dt)
+            % 前進オイラー法による時間発展
+            dx = obj.dx_func(x_pre, pre_input, obj.physical_param);
+            x_plus = x_pre + dx*dt;
+        end
+
+        function x_plus = RK4(obj, x_pre, pre_input, dt)
+            % 4次ルンゲ・クッタ法による時間発展
+            dx = @(x) obj.dx_func(x, pre_input, obj.physical_param);
+
+            k1 = dx(x_pre);
+            k2 = dx(x_pre + dt*k1/2);
+            k3 = dx(x_pre + dt*k2/2);
+            k4 = dx(x_pre + dt*k3);
+
+            x_plus = x_pre + dt/6*(k1 + 2*k2 + 2*k3 + k4);
+        end
+
+
+
+        function Delta_input = infer_model(obj, plant_state, nominal_state)
+            % モデルの推論を行う
+            % [Inputs]
+            %   plant_state: [p; q; v; w] (12,:)
+            %   nominal_state: [p; q; v; w] (12,:)
+            %
+            % [Outputs]
+            %   Delta_input: 推論結果 (4,:)
+            %   実際にdoループで使うときには plant_input = nominal_input + Delta_input となる
+
+            size_plant = size(plant_state);
+            size_nonimal = size(nominal_state);
+            if size_plant(1)~=size_nonimal(1) || size_plant(2)~=size_nonimal(2)
+                msg = "plantとnominalのサイズが一致しません\n" + ...
+                    "plant size:   " + num2str(size_plant) + "\n" + ...
+                    "nominal size: " + num2str(size_nonimal) + "\n\n";
+                error(sprintf(msg));
+            end
+
+            Delta_input = zeros(4,size_plant(2));
+            for row = 1:size_plant(2)
+                data = obj.gen_data_func(plant_state(:,row), nominal_state(:,row));
+                Delta_input(:,row) = -1*double(predict(obj.NNMEC_model, data'))'; % predict関数での推論
+            end
+        end
+    end
+end
---
+++ reference/functions/way_point_ref.m
@@ -0,0 +1,153 @@
+    %スプラインの係数計算
+            function ref = way_point_ref(val,n,check)
+            % val         %時間とwaypoint
+            arguments
+                val         %時間とwaypoint
+                n           %多項式次数
+                check       %事前に軌道確認
+            end
+            time = val(:,1)';
+            point = val(:,2:end)';
+            dtime = diff(time');%隣の点との差 dw_i = w_i - w_i-1 (dw_1 = 0),  i=1,2,3,...
+            Sn=length(time(1:end-1)); %求める多項式の数
+            D(1,:)=ones(1,n+1);%多項式の係数行列1に初期化
+            
+            for i = 1:n-1%多項式の階数ごとの微分係数計算
+                D(i+1,:)=[zeros(1,i), 1:n-i+1].*D(i,:);
+                % D(i+1,:)=[zeros(1,i), polyder(D(i,i:end))];
+            end
+            %微分した時の係数
+            D_ori=D;
+            D=D./factorial(0:n-1)';
+            
+            power_dtime=zeros(Sn,n+1);
+            for i=1:n+1
+                power_dtime(:,i) = dtime.^(i-1);
+            end
+            %各ポイント間のrefenceの関数の係数を行列まとめて計算
+            %P=Xp^(-1)Y
+            %P:求める係数
+            %Xp:時間とt^nの微分係数の積の行列
+            %Y:waypoint
+            %Xpの生成
+            X=zeros((n+1)*Sn);
+            
+            for i=1:Sn
+                dr=(i-1)*2;
+                dc=(i-1)*(n+1);
+                X(1+dr:2+dr,1+dc:(n+1)+dc) = [1,zeros(1,n);power_dtime(i,:)];
+            end
+            
+            poweT = zeros(n-1,n+1);
+            for i = 1:Sn-1
+                for j=1:n-1
+                    poweT(j,j+1:end) = power_dtime(i,1:end-j) ;
+                end
+                dr2=(i-1)*(n-1);
+                dc2=(i-1)*(n+1);
+                X(2*Sn+1+dr2 : 2*Sn+n-1+dr2, 1+dc2 : (n+1)+dc2) = D(2 : end,1:end).*poweT;
+                X(2*Sn+1+dr2 : 2*Sn+n-1+dr2, n+3+dc2 : 2*n+1 +dc2) = - eye(n-1);
+            end
+            add=(n+1)*Sn - (n-1);
+            for j=1:n-1
+                    poweT(j,j+1:end) = power_dtime(end,1:end-j) ;
+            end
+            %端点の制約
+            for i = 1:n-2
+                dr3=(i-1)*2;
+                % X(add+1+dr3,1+i) = 1;%4関数でさいしょの端点で激しくなるか，最後収束しないかでインデックス1,2をへんこう
+                % X(add+2+dr3,end-n:end) = D(i+1,:).*poweT(i,:) ;
+                X(add+2+dr3,1+i) = 1;%さいしょの端点で激しくなるか，最後収束しないかでインデックス1,2をへんこう
+                X(add+1+dr3,end-n:end) = D(i+1,:).*poweT(i,:) ;
+            end
+            
+            s=1:(n+1)*Sn;
+            Xp =X(s,s);
+    
+            %P=Xp^(-1)Yの計算
+            P = zeros(3,n+1,Sn);
+            for i = 1:3
+                Y1 =  reshape(ones(2,Sn+1).*point(i, :),2*(Sn+1),1);
+                Y1 = Y1(2:end-1);
+                Y = [Y1;zeros((n+1)*Sn-2*Sn,1)];
+                P(i,:,:) = reshape(Xp\Y,1,n+1,Sn);
+            end
+            
+            %係数の格納
+            co=[];
+            for i = 0:n
+            co = [co,"d"+num2str(i)];
+            end
+            index = length(co);
+            for i = 1:index
+                coefficients.(co(i))=zeros(3,n+2-i ,Sn);
+            end
+            
+            coefficients.d0 = P;
+            if length(co) > n 
+                index = n;
+            end
+            for i = 2:index
+                coefficients.(co(i))=coefficients.d0(:,i:end,:).*D_ori(i,i:end);
+            end
+            ref.n=n;
+            ref.coefficients=coefficients;
+            ref.t=time(2:end);
+            ref.dt=dtime;
+            
+            names = fieldnames(ref.coefficients);            
+            for i = 1:length(names)
+                t_powers.(names{i}) = @(t) (t).^(0:n+1-i)';
+            end
+            ref.t_powers = t_powers;
+            ref.time = time;
+            %一般式作る
+            ref.period = time(end);%軌道の周期
+            ref.Sn = Sn;%区間数
+            
+            if check == 1   %軌道事前確認※2024年度のそのまま移植
+            t_ref0=0;
+            i=1;
+            j=1;
+            delta=0.025;
+            end_time=time(end)+1;
+            length_time = length(0:delta:end_time);
+            xyz = zeros(3,length_time);
+            for t_f = 0:delta:end_time
+                t_ref= t_f - t_ref0;%目標地点が定められた時間からの経過時間
+                if round(t_ref,4) >= dtime(i) 
+                   i=i+1;
+                    if i >length(ref.t) 
+                        i = length(ref.t);
+                        t_ref = dtime(end);
+                    else
+                        t_ref0 = round(t_f,4);
+                        t_ref=0;
+                    end
+                end
+                xyz(:,j) = coefficients.(names{1})(:,:,i)*t_powers.(names{1})(t_ref);
+                j=j+1;
+            end
+            
+            figure(101)
+            clf
+            plot3(xyz(1,:), xyz(2,:), xyz(3,:), 'LineWidth', 2)
+            hold on
+            grid on
+            plot3(val(1:end,2),val(1:end,3),val(1:end,4), 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
+            h_start = plot3(val(1,2), val(1,3), val(1,4), 'gp', 'MarkerSize', 12, 'MarkerFaceColor', 'g');
+            xlabel('$x$ [m]', 'FontSize', 14, 'Interpreter', 'latex')
+            ylabel('$y$ [m]', 'FontSize', 14, 'Interpreter', 'latex')
+            zlabel('$z$ [m]', 'FontSize', 14, 'Interpreter', 'latex')
+            title('Check reference', 'FontSize', 14)
+            set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 14)
+            axis equal
+            legend(h_start, {'Start Point'},  'FontSize', 14)
+            
+            
+            end
+
+
+
+        end
+   
