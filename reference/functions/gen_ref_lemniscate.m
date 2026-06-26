3e05c9c59d58dc8889b63c9ed9fd3ae4b44e426d
YosukeKoseki <2212044y.koseki@gmail.com>
2026-06-26 10:25:43 +0900

    merge処理

---
+++ reference/functions/gen_ref_lemniscate.m
@@ -0,0 +1,28 @@
+function ref = gen_ref_lemniscate(param)
+arguments
+    param.freq = 10% 周期
+    param.center = [0 0 1]% 円の中心
+    param.radius = 1.0 % 長軸方向の大きさ
+    param.phase = 0.0 % 位相
+    param.x = 1 % x方向がレム二スケート∞の長軸
+end
+x_0=param.center(1);
+y_0=param.center(2);
+z_0=param.center(3);
+T = param.freq;
+a = param.radius;
+phase = param.phase;
+
+if param.x == 1 % x方向に∞
+    ref=@(t) [  x_0 + a*sin(2*pi*t/T+phase)/(1+cos(2*pi*t/T+phase).^2); %x
+                y_0 + a*sin(2*pi*t/T+phase)*cos(2*pi*t/T+phase)/(1+cos(2*pi*t/T+phase).^2); %y
+                z_0; % z
+                -pi/2]; % yaw
+    
+else            % y方向に∞
+    ref=@(t) [  x_0 + a*sin(2*pi*t/T+phase)*cos(2*pi*t/T+phase)/(1+cos(2*pi*t/T+phase).^2); %x
+                y_0 + a*sin(2*pi*t/T+phase)/(1+cos(2*pi*t/T+phase).^2); %y
+                z_0; %z
+                0]; % yaw
+end
+end
