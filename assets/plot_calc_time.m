4665d22d553199bd2ad77cad764e350f97dd3d9e
YosukeKoseki <2212044y.koseki@gmail.com>
2026-06-26 15:05:01 +0900

    plot_calc_time修正
    ・引数の順番
    ・tの取り方
    ・フェーズ切り替え時の時間も縦線でplot

--- assets/plot_calc_time.m
+++ assets/plot_calc_time.m
@@ -1,4 +1,4 @@
-function plot_calc_time(logger, LW, FS, set_dt)
+function plot_calc_time(logger, set_dt, LW, FS)
 %check_calc_time 計算時間を確認・プロットする関数
 %   logger: LOGGERクラス
 %   LW: LineWidth
@@ -6,27 +6,30 @@ function plot_calc_time(logger, LW, FS, set_dt)
 %   dt: 制御周期
 arguments
     logger
+    set_dt = 0.025;
     LW = 1.0;
     FS = 16;
-    set_dt = 0.025;
 end
 
 set_dt_ms = set_dt*10^3;
-t0id = find(logger.Data.phase==97,1,'last')+1;
-teid = find(logger.Data.phase==0,1,'first')-1;
-dt = diff(logger.Data.t(t0id:teid));
-t = logger.Data.t(t0id:teid-1);
+t=logger.data('t',"","");
+dt = diff(t);
+dt = dt.*10^3;
+
+t_flight = logger.Data.t(find(logger.Data.phase=='t',1,'last'));
+t_land   = logger.Data.t(find(logger.Data.phase=='f',1,'last'));
 figure(112)
 ax = gca;
-plot(t,dt*10^3, Linewidth=LW);
+plot(t(1:end-1),dt, Linewidth=LW);
 hold on
 yline(set_dt_ms,"LineWidth",LW) % 制御周期プロット
+xline(t_flight, '--'); xline(t_land, '--');
 hold off
 grid on; grid minor;
 
 % leg_txt = "Control period setting" + " ("+string(set_dt_ms)+" ms)";
 leg_txt = "Control period setting";
-legend("Actual computation time", leg_txt, "Location","southeast")
+legend("Actual computation time", leg_txt, "Location","best")
 set(ax.XAxis, fontsize=FS-2)
 set(ax.YAxis, fontsize=FS-2)
 set(ax.Legend, 'FontSize',FS-4);
