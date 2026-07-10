25b1699d615bc3de407ca2c6431ed1a42edf1664
my2312122-dot <my2312122@gmail.com>
2026-07-10 16:29:11 +0900

    KL調整中
    全部のフォルダに実行できない問題進行中

---
+++ Data/check_data.m
@@ -0,0 +1,63 @@
+clear;
+clc;
+
+%% 複数のMATファイルを選択
+[fileNames, filePath] = uigetfile( ...
+    {'*.mat', 'MAT-files (*.mat)'; ...
+     '*.*',   'すべてのファイル (*.*)'}, ...
+    '確認したいMATファイルを選択', ...
+    'MultiSelect', 'on');
+
+%% キャンセルした場合
+if isequal(fileNames, 0)
+    disp('ファイル選択がキャンセルされました');
+    return;
+end
+
+%% 1ファイルだけ選択した場合もセル配列に変換
+if ischar(fileNames)
+    fileNames = {fileNames};
+end
+
+%% 選択したファイルを順番に確認
+for fileIdx = 1:length(fileNames)
+
+    fileName = fileNames{fileIdx};
+    fullFileName = fullfile(filePath, fileName);
+
+    %% ファイル読み込み
+    data = load(fullFileName);
+
+    %% ファイル名表示
+    fprintf('\n\n');
+    fprintf('========================================\n');
+    fprintf('ファイル名：%s\n', fileName);
+    fprintf('========================================\n');
+
+    %% 変数一覧
+    info = whos('-file', fullFileName);
+
+    fprintf('\n=== 変数一覧 ===\n');
+
+    for i = 1:length(info)
+        fprintf('%s    size: %s    class: %s\n', ...
+            info(i).name, ...
+            mat2str(info(i).size), ...
+            info(i).class);
+    end
+
+    %% 各変数の中身
+    fprintf('\n=== 変数の中身 ===\n');
+
+    variableNames = fieldnames(data);
+
+    for i = 1:length(variableNames)
+
+        variableName = variableNames{i};
+
+        fprintf('\n----- %s -----\n', variableName);
+        disp(data.(variableName));
+
+    end
+
+end
