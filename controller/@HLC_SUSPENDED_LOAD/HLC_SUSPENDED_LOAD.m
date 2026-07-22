8175e55564d99d8c9cc8b79804ece559a996a9ed
Tanaka Akito <166685166+Atanaka-tcu@users.noreply.github.com>
2026-07-22 19:49:36 +0900

    notchフィルターの搭載

--- controller/@HLC_SUSPENDED_LOAD/HLC_SUSPENDED_LOAD.m
+++ controller/@HLC_SUSPENDED_LOAD/HLC_SUSPENDED_LOAD.m
@@ -1,91 +1,124 @@
 classdef HLC_SUSPENDED_LOAD < handle
-% クアッドコプター用階層型線形化を使った入力算出
-properties
-    self
-    result
-    param
-end
-
-methods
-
-    function obj = HLC_SUSPENDED_LOAD(self, param)
-        obj.self = self;
-        obj.param = param;
-
+    % クアッドコプター用階層型線形化を使った入力算出
+    properties
+        self
+        result
+        param
+        notchx
+        notchy
     end
 
-    function result = do(obj, varargin)
-        Param = obj.param; % param (optional) : 構造体：ゲインF1-F4
-        model = obj.self.estimator.result; % 推定した状態
-        ref = obj.self.reference.result; % 目標値
+    methods
 
-        % 目標値を取得
-        if isprop(ref.state, 'xd')
-            xd = ref.state.xd; % 20次元の目標値に対応する用
-        else
-            xd = ref.state.get();
+        function obj = HLC_SUSPENDED_LOAD(self, param)
+            obj.self = self;
+            obj.param = param;
+
         end
 
-        pL = model.state.pL;
-        if isprop(model.state, "pT")
-            pT = model.state.pT;
-        else
-            delta = pL - model.state.p;
-            if norm(delta) > 1e-9
-                pT = delta / norm(delta);
+        function result = do(obj, varargin)
+            Param = obj.param; % param (optional) : 構造体：ゲインF1-F4
+            model = obj.self.estimator.result; % 推定した状態
+            ref = obj.self.reference.result; % 目標値
+
+            % 目標値を取得
+            if isprop(ref.state, 'xd')
+                xd = ref.state.xd; % 20次元の目標値に対応する用
             else
-                pT = [0; 0; -1];
+                xd = ref.state.get();
             end
+
+            pL = model.state.pL;
+            if isprop(model.state, "pT")
+                pT = model.state.pT;
+            else
+                delta = pL - model.state.p;
+                if norm(delta) > 1e-9
+                    pT = delta / norm(delta);
+                else
+                    pT = [0; 0; -1];
+                end
+            end
+            P = [obj.self.parameter.get(["mass", "jx", "jy", "jz", "gravity", "loadmass", "cableL"]), 0, 0];
+
+            x = [model.state.getq('compact'); model.state.w; pL; model.state.vL; pT; model.state.wL]; % [q, w ,pL, vL, pT, wL]に並べ替え
+            % [model.state.p, x(8:10), xd(1:3), x(8:10) - xd(1:3)]
+            % yaw角の定義域の問題を回避,h4 = yaw - yawd(誤差)だがyawd = -(誤差)+yawの値を入れる．x,y,yawの仮想入力はVs_SuspendedLoadはクオータニオンで計算するため
+            % yawサブシステムの入力を設計するときにyaw角を打ち消して定義域修正した誤差を反映
+            yaw = wrapToPi(model.state.q(3)); % 機体yaw角[-pi,pi]にする特にyaw
+            yawd = xd(4); % 目標yaw角
+            yawUnit = [cos(yaw); sin(yaw); 0]; % yawの方向ベクトル
+            yawdUnit = [cos(yawd); sin(yawd); 0]; % yawdの方向ベクトル
+            deltaYaw = sign(cross(yawdUnit, yawUnit)) * acos(yawdUnit' * yawUnit); % 目標角度からみた機体角度との誤差
+            xd(4) = -deltaYaw(3) + yaw; % yaw打ち消しと誤差をyawの目標角に入れる．
+            %目標値の格納
+            xd = [xd; zeros(28 - size(xd, 1), 1)]; % 足りない分は0で埋める．
+
+            % 階層型線形化による入力計算
+            % 仮想入力のゲイン
+            F1 = Param.F1; % z方向サブシステムのゲイン
+            F2 = Param.F2; % x方向サブシステムのゲイン
+            F3 = Param.F3; % y方向サブシステムのゲイン
+            F4 = Param.F4; % yaw方向サブシステムのゲイン
+
+            vf = obj.Vfd_SuspendedLoadxyDst(Param.dt, x, xd', F1); % 実験で刻み時間が変わったときに対応
+            vs = obj.Vs_SuspendedLoadxyDst(x, xd', vf, P, F2, F3, F4); % 第二層x,y,yawサブシステムの仮想入力の計算
+            uf = obj.Uf_SuspendedLoadxyDst(x, xd', vf, P); % 第一層の仮想入力の実入力(推力)への変換
+            % h234  = obj.H234_SuspendedLoadxyDst(x,xd',vf,P);  % ただの単位行列なのでなくてもいい
+            %tic
+            beta2 = obj.Beta2_SuspendedLoadxyDst(x, xd', vf, P); % 第二層のbetaの逆行列
+            vs_alpha2 = obj.V2_alpha2_SuspendedLoadxyDst(x, xd', vf, vs', P); % 第二層のvs - alpha
+            us = beta2 \ vs_alpha2; % 第二層の実入力（roll,pitch,yawのトルク）への変換：bate^(-1)*(vs - alpha) %h234*invbeta2*a2;
+            %obj.result.aa=toc;
+            tmp = [uf(1); us]; % 実入力へ変換
+
+            tmp = obj.notchFilter(tmp, Param.dt);   % ノッチフィルタ適用
+
+            obj.result.tmp = tmp; % 入力に制限を付けてない値を格納
+
+            % 安全のため入力値に制限を付ける．推定した牽引物質量や紐の長さ，外乱などを表示．
+            %disp("time: "+ num2str(t,2)+" z position of drone:
+            %"+num2str(model.state.p(3),3)+" estimated load mass:
+            %"+num2str(P(6),4)+" dst:(x,y) "+num2str(P(end-1:end),4))
+            obj.result.input = [max(0, min(20, tmp(1))); max(-1, min(1, tmp(2))); max(-1, min(1, tmp(3))); max(-1, min(1, tmp(4)))]; %+[normrnd(0,0.01,1);normrnd(0,0.001,[3,1])]*1; %入力にノイズを付与可能
+            % obj.result.input = [0.0,0,0,(0.5236+0.04)*9.81]';%
+            obj.result.xd = xd;
+            obj.result.x = x;
+            result = obj.result;
+
         end
-        P = [obj.self.parameter.get(["mass", "jx", "jy", "jz", "gravity", "loadmass", "cableL"]), 0, 0];
 
-        x = [model.state.getq('compact'); model.state.w; pL; model.state.vL; pT; model.state.wL]; % [q, w ,pL, vL, pT, wL]に並べ替え
-        % [model.state.p, x(8:10), xd(1:3), x(8:10) - xd(1:3)]
-        % yaw角の定義域の問題を回避,h4 = yaw - yawd(誤差)だがyawd = -(誤差)+yawの値を入れる．x,y,yawの仮想入力はVs_SuspendedLoadはクオータニオンで計算するため
-        % yawサブシステムの入力を設計するときにyaw角を打ち消して定義域修正した誤差を反映
-        yaw = wrapToPi(model.state.q(3)); % 機体yaw角[-pi,pi]にする特にyaw
-        yawd = xd(4); % 目標yaw角
-        yawUnit = [cos(yaw); sin(yaw); 0]; % yawの方向ベクトル
-        yawdUnit = [cos(yawd); sin(yawd); 0]; % yawdの方向ベクトル
-        deltaYaw = sign(cross(yawdUnit, yawUnit)) * acos(yawdUnit' * yawUnit); % 目標角度からみた機体角度との誤差
-        xd(4) = -deltaYaw(3) + yaw; % yaw打ち消しと誤差をyawの目標角に入れる．
-        %目標値の格納
-        xd = [xd; zeros(28 - size(xd, 1), 1)]; % 足りない分は0で埋める．
+        function show(obj)
+            obj.result
+        end
 
-        % 階層型線形化による入力計算
-        % 仮想入力のゲイン
-        F1 = Param.F1; % z方向サブシステムのゲイン
-        F2 = Param.F2; % x方向サブシステムのゲイン
-        F3 = Param.F3; % y方向サブシステムのゲイン
-        F4 = Param.F4; % yaw方向サブシステムのゲイン
+        function tmp_f = notchFilter(obj, tmp, dt)
+            if isempty(obj.notchx)
+                g = obj.self.parameter.get("gravity");
+                L = obj.self.parameter.get("cableL");   % ケーブル長を直接取得
+                wn = 2*pi*sqrt(g/L); %中心周波数を設計
+                % fprintf('L = %.4f, wn = %.4f rad/s\n', L, wn);
+                zz = [0.3, 0.3]; %ノッチの深さを設定 [roll, pitch]
+                zp = [0.9, 0.9]; %ノッチの幅を設定 [roll, pitch]
 
-        vf = obj.Vfd_SuspendedLoadxyDst(Param.dt, x, xd', F1); % 実験で刻み時間が変わったときに対応
-        vs = obj.Vs_SuspendedLoadxyDst(x, xd', vf, P, F2, F3, F4); % 第二層x,y,yawサブシステムの仮想入力の計算
-        uf = obj.Uf_SuspendedLoadxyDst(x, xd', vf, P); % 第一層の仮想入力の実入力(推力)への変換
-        % h234  = obj.H234_SuspendedLoadxyDst(x,xd',vf,P);  % ただの単位行列なのでなくてもいい
-        %tic
-        beta2 = obj.Beta2_SuspendedLoadxyDst(x, xd', vf, P); % 第二層のbetaの逆行列
-        vs_alpha2 = obj.V2_alpha2_SuspendedLoadxyDst(x, xd', vf, vs', P); % 第二層のvs - alpha
-        us = beta2 \ vs_alpha2; % 第二層の実入力（roll,pitch,yawのトルク）への変換：bate^(-1)*(vs - alpha) %h234*invbeta2*a2;
-        %obj.result.aa=toc;
-        tmp = [uf(1); us]; % 実入力へ変換
-        obj.result.tmp = tmp; % 入力に制限を付けてない値を格納
+                % roll用フィルタ設計
+                dx = c2d(tf([1 2*zz(1)*wn wn^2], [1 2*zp(1)*wn wn^2]), dt, 'tustin');%tfを使って伝達関数を作成
+                %c2d(..., dt, 'tustin')連続系の伝達関数をdtをもとに離散系に変換
+                [bx, ax] = tfdata(dx, 'v'); %伝達関数の係数を数値ベクトルで保管保管
+                zx0 = zeros(max(length(ax), length(bx)) - 1, 1); %メモリの初期化
+                obj.notchx = struct('b', bx, 'a', ax, 'z', zx0);
 
-        % 安全のため入力値に制限を付ける．推定した牽引物質量や紐の長さ，外乱などを表示．
-        %disp("time: "+ num2str(t,2)+" z position of drone:
-        %"+num2str(model.state.p(3),3)+" estimated load mass:
-        %"+num2str(P(6),4)+" dst:(x,y) "+num2str(P(end-1:end),4))
-        obj.result.input = [max(0, min(20, tmp(1))); max(-1, min(1, tmp(2))); max(-1, min(1, tmp(3))); max(-1, min(1, tmp(4)))]; %+[normrnd(0,0.01,1);normrnd(0,0.001,[3,1])]*1; %入力にノイズを付与可能
-        % obj.result.input = [0.0,0,0,(0.5236+0.04)*9.81]';%
-        obj.result.xd = xd;
-        obj.result.x = x;
-        result = obj.result;
+                % pitch用フィルタ設計
+                dy = c2d(tf([1 2*zz(2)*wn wn^2], [1 2*zp(2)*wn wn^2]), dt, 'tustin');
+                [by, ay] = tfdata(dy, 'v');
+                zy0 = zeros(max(length(ay), length(by)) - 1, 1);
+                obj.notchy = struct('b', by, 'a', ay, 'z', zy0);
+            end
 
+            tmp_f = tmp;
+            [tmp_f(2), obj.notchx.z] = filter(obj.notchx.b, obj.notchx.a, tmp(2), obj.notchx.z);   % roll　 入力信号 x をフィルタ処理した結果が得られる
+            %入力：フィルタの分子係数、フィルタの分母係数、フィルタにかける入力信号、前回のフィルタ処理後に保存しておいた遅延要素の値
+            [tmp_f(3), obj.notchy.z] = filter(obj.notchy.b, obj.notchy.a, tmp(3), obj.notchy.z);   % pitch
+        end
     end
-
-    function show(obj)
-        obj.result
-    end
-end
-
 end
