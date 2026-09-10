d7bf1fb21c247f7aebd15f20a70c48b374b1c178
Tanaka Akito <166685166+Atanaka-tcu@users.noreply.github.com>
2026-09-10 17:44:28 +0900

    ノッチ変更

--- controller/@HLC_SUSPENDED_LOAD/HLC_SUSPENDED_LOAD.m
+++ controller/@HLC_SUSPENDED_LOAD/HLC_SUSPENDED_LOAD.m
@@ -43,7 +43,7 @@ classdef HLC_SUSPENDED_LOAD < handle
                 end
             end
             P = [obj.self.parameter.get(["mass", "jx", "jy", "jz", "gravity", "loadmass", "cableL"]), 0, 0];
-
+            P(7)=1.2 ;
             x = [model.state.getq('compact'); model.state.w; pL; model.state.vL; pT; model.state.wL]; % [q, w ,pL, vL, pT, wL]に並べ替え
             % [model.state.p, x(8:10), xd(1:3), x(8:10) - xd(1:3)]
             % yaw角の定義域の問題を回避,h4 = yaw - yawd(誤差)だがyawd = -(誤差)+yawの値を入れる．x,y,yawの仮想入力はVs_SuspendedLoadはクオータニオンで計算するため
@@ -56,7 +56,12 @@ classdef HLC_SUSPENDED_LOAD < handle
             xd(4) = -deltaYaw(3) + yaw; % yaw打ち消しと誤差をyawの目標角に入れる．
             %目標値の格納
             xd = [xd; zeros(28 - size(xd, 1), 1)]; % 足りない分は0で埋める．
+<<<<<<< Updated upstream
             obj.result.xdresult=xd;
+=======
+            % tt=obj.notchFilter([0;xd(1:3)], Param.dt);
+            % xd(1)=tt(2);
+>>>>>>> Stashed changes
 
             % 階層型線形化による入力計算
             % 仮想入力のゲイン
@@ -67,6 +72,8 @@ classdef HLC_SUSPENDED_LOAD < handle
 
             vf = obj.Vfd_SuspendedLoadxyDst(Param.dt, x, xd', F1); % 実験で刻み時間が変わったときに対応
             vs = obj.Vs_SuspendedLoadxyDst(x, xd', vf, P, F2, F3, F4); % 第二層x,y,yawサブシステムの仮想入力の計算
+            % v= obj.notchFilter([0;vs'], Param.dt);
+            % vs(1)=v(2);
             uf = obj.Uf_SuspendedLoadxyDst(x, xd', vf, P); % 第一層の仮想入力の実入力(推力)への変換
             % h234  = obj.H234_SuspendedLoadxyDst(x,xd',vf,P);  % ただの単位行列なのでなくてもいい
             %tic
@@ -74,8 +81,13 @@ classdef HLC_SUSPENDED_LOAD < handle
             vs_alpha2 = obj.V2_alpha2_SuspendedLoadxyDst(x, xd', vf, vs', P); % 第二層のvs - alpha
             us = beta2 \ vs_alpha2; % 第二層の実入力（roll,pitch,yawのトルク）への変換：bate^(-1)*(vs - alpha) %h234*invbeta2*a2;
             %obj.result.aa=toc;
+<<<<<<< Updated upstream
             % obj.cha = varargin{2};
             % 
+=======
+            obj.cha = varargin{2};
+
+>>>>>>> Stashed changes
             % if obj.cha=="f"
             %     % chirp ノイズ
             %     %基本設定
@@ -90,7 +102,14 @@ classdef HLC_SUSPENDED_LOAD < handle
             %     t_end=varargin{1}.te;
             % 
             %     %pitch,roll 統一
+<<<<<<< Updated upstream
             %     u=1e-1*chirp(t_now, 0, t_end, 0.25, [], -90 );
+=======
+            %     u=0.02*chirp(t_now, 0.05, t_end, 3.0 , "logarithmic" );
+            %     %%f_lo = 0.05;  f_hi = 3.0;             % [Hz]
+            %     %%tau_amp = 0.02;                       % 励振トルク振幅 [N*m]（小さめ・線形域）
+            %     %%probe = tau_amp * chirp(tvec, f_lo, te, f_hi, "logarithmic");
+>>>>>>> Stashed changes
             %     % u=1e-1 * sin(2*pi*0.2*t_now);
             %     % u=5e-3 * (sin(2*pi*0.25*t_now)+t_now)+-5e-2;
             %     obj.result.chirp=u;
@@ -105,11 +124,19 @@ classdef HLC_SUSPENDED_LOAD < handle
             %     % us(1) = us(1)+u_pitch;
             %     % us(2) = us(2)+u_roll;
             % end
+<<<<<<< Updated upstream
 
             tmp = [uf(1); us]; % 実入力へ変換
 
             % tmp = obj.notchFilter(tmp, Param.dt);   % ノッチフィルタ適用
 
+=======
+            % 
+            tmp = [uf(1); us]; % 実入力へ変換
+            % tmp(3)
+            tmp = obj.notchFilter(tmp, Param.dt);   % ノッチフィルタ適用
+            % tmp(3)
+>>>>>>> Stashed changes
             obj.result.tmp = tmp; % 入力に制限を付けてない値を格納
 
             % 安全のため入力値に制限を付ける．推定した牽引物質量や紐の長さ，外乱などを表示．
@@ -132,10 +159,12 @@ classdef HLC_SUSPENDED_LOAD < handle
             if isempty(obj.notchx)
                 g = obj.self.parameter.get("gravity");
                 L = obj.self.parameter.get("cableL");   % ケーブル長を直接取得
-                wn = 2*pi*sqrt(g/L); %中心周波数を設計
+                % wn = 1.57; %中心周波数を設計
+                % wn = sqrt(g/3.0); %中心周波数を設計
+                wn=-1;
                 % fprintf('L = %.4f, wn = %.4f rad/s\n', L, wn);
-                zz = [0.3, 0.3]; %ノッチの深さを設定 [roll, pitch]
-                zp = [0.9, 0.9]; %ノッチの幅を設定 [roll, pitch]
+                zz = [0.001, 0.01]; %ノッチの深さを設定 [roll, pitch]
+                zp = [0.5, 1]; %ノッチの幅を設定 [roll, pitch]
 
                 % roll用フィルタ設計
                 dx = c2d(tf([1 2*zz(1)*wn wn^2], [1 2*zp(1)*wn wn^2]), dt, 'tustin');%tfを使って伝達関数を作成
@@ -152,9 +181,10 @@ classdef HLC_SUSPENDED_LOAD < handle
             end
 
             tmp_f = tmp;
-            [tmp_f(2), obj.notchx.z] = filter(obj.notchx.b, obj.notchx.a, tmp(2), obj.notchx.z);   % roll　 入力信号 x をフィルタ処理した結果が得られる
+            % [tmp_f(2), obj.notchx.z] = filter(obj.notchx.b, obj.notchx.a, tmp(2), obj.notchx.z);   % roll　 入力信号 x をフィルタ処理した結果が得られる
             %入力：フィルタの分子係数、フィルタの分母係数、フィルタにかける入力信号、前回のフィルタ処理後に保存しておいた遅延要素の値
-            [tmp_f(3), obj.notchy.z] = filter(obj.notchy.b, obj.notchy.a, tmp(3), obj.notchy.z);   % pitch
+            [tmp_f(3), obj.notchy.z] = filter(obj.notchy.b, obj.notchy.a, tmp(3), obj.notchy.z); % pitch
+            % obj.notchy.z
         end
     end
 end
